728168675b3968bef447b472336303ba85d019de
tachu <roatyungtatyu@gmail.com>
2024-11-10 18:10:19 +0900

    add: Notes in db

--- learning/tterao/mynotes/notes/models.py
+++ learning/tterao/mynotes/notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Notes(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
