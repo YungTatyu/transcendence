f47627458f6e4b3f277f67b3a6f4aae0d2e3bd95
tachu <roatyungtatyu@gmail.com>
2024-11-10 20:35:56 +0900

    add: urls

--- learning/tterao/mynotes/mynotes/urls.py
+++ learning/tterao/mynotes/mynotes/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from notes.views import NoteListView, NoteCreateView, NoteUpdateView, NoteDeleteView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", NoteListView.as_view(), name="home"),
+    path("notes", NoteListView.as_view(), name="note_list"),
+    path("notes/add", NoteCreateView.as_view(), name="note_add"),
+    path("notes/<int:id>/edit", NoteUpdateView.as_view(), name="note_edit"),
+    path("notes/<int:id>/delete", NoteDeleteView.as_view(), name="note_delete"),
 ]
