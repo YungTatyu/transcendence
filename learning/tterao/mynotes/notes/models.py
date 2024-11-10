ebbed67aa022b39b1d549d81cdfcfaf198052907
tachu <roatyungtatyu@gmail.com>
2024-11-10 20:36:28 +0900

    modify: Notes to Note

--- learning/tterao/mynotes/notes/models.py
+++ learning/tterao/mynotes/notes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Notes(models.Model):
+class Note(models.Model):
     title = models.CharField(max_length=20)
     content = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
--- learning/tterao/mynotes/notes/views.py
+++ learning/tterao/mynotes/notes/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
+from .models import Note
 
-# Create your views here.
+
+class NoteListView(ListView):
+    model = Note
+    template_name = "notes/note_list.html"
+
+
+class NoteCreateView(CreateView):
+    model = Note
+    template_name = "notes/note_form.html"
+    fields = ["title", "content"]
+    success_url = reverse_lazy("note_list")
+
+
+class NoteUpdateView(UpdateView):
+    model = Note
+    template_name = "notes/note_form.html"
+    fields = ["title", "content"]
+    success_url = reverse_lazy("note_list")
+
+
+class NoteDeleteView(DeleteView):
+    model = Note
+    success_url = reverse_lazy("note_list")
