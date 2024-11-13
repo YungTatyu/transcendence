93d8a595198fe2dc1e1c88e674e950d4574806af
tachu <roatyungtatyu@gmail.com>
2024-11-13 13:43:50 +0900

    add: now be able to call apis

--- learning/tterao/mynotes_rest/notes/views.py
+++ learning/tterao/mynotes_rest/notes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import User, Note
+from rest_framework import permissions, viewsets
+from .serializers import UserSerializer, NoteSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+
+    queryset = Note.objects.all().order_by("created_at")
+    serializer_class = NoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- learning/tterao/mynotes_rest/notes_bonus/settings.py
+++ learning/tterao/mynotes_rest/notes_bonus/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "notes",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 AUTH_USER_MODEL = "notes.User"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
