6f0f9d7c729d123335c6eb50d845c58f0bba6f35
rikeda-cloud <longsichitian648@gmail.com>
2024-11-18 19:52:13 +0900

    [add] Simple WebSocket Chat App

---
+++ websocket_demo/websocket_demo/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for websocket_demo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from chat.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websocket_demo.settings')
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
