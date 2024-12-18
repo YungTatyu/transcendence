d9e90ebcced1d704a3120a804a2b567180d45588
tachu <roatyungtatyu@gmail.com>
2024-12-19 00:10:47 +0900

    add: make lint, make fmt

--- Makefile
+++ Makefile
@@ -1,10 +1,20 @@
 
 LINTER_DIR := linters
 LINTER_IMG_NAME := linter-formatter
+DOCKER_MNT_DIR := /app
 
-check:
+.PHONY: build-linter
+build-linter:
 	docker build -t ${LINTER_IMG_NAME} -f ${LINTER_DIR}/Dockerfile .
-	docker run --rm -it ${LINTER_IMG_NAME} 
 
+.PHONY: lint
+lint: build-linter
+	docker run -it --rm -v .:${DOCKER_MNT_DIR} ${LINTER_IMG_NAME} lint
+
+.PHONY: fmt
+fmt: build-linter
+	docker run -it --rm -v .:${DOCKER_MNT_DIR} ${LINTER_IMG_NAME} fmt
 
 .PHONY: check
+check: build-linter
+	docker run -it --rm -v .:${DOCKER_MNT_DIR} ${LINTER_IMG_NAME}
---
+++ linters/entrypoint.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+lint() {
+  local option="$1"
+  if [[ ${option} != "py" ]]; then
+    biome lint --write --error-on-warnings
+  fi
+  if [[ ${option} != "js" ]]; then
+    ruff check --fix
+  fi
+  return 0
+}
+
+fmt() {
+  local option="$1"
+  if [[ ${option} != "py" ]]; then
+    biome format --write
+  fi
+  if [[ ${option} != "js" ]]; then
+    ruff format
+  fi
+  return 0
+}
+
+main() {
+  local cmd="$1"
+  local option="$2"
+  if [[ ${cmd} == "lint" ]]; then
+    lint ${option}
+    return 0
+  fi
+  if [[ ${cmd} == "fmt" ]]; then
+    fmt ${option}
+    return 0
+  fi
+
+  # jsのlinterとformatterを実行
+  biome check --error-on-warnings || return 1
+
+  # puthonのlinterとformatterを実行
+  ruff check || return 1
+  ruff format --diff || return 1
+  return 0
+}
+
+main "$@"
