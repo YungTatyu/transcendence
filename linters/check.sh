d6f202a30a92210d98a4ed4fbcbc7c30e68726c8
tachu <roatyungtatyu@gmail.com>
2024-12-18 23:16:01 +0900

    add: js formatter, linter

--- linters/check.sh
+++ linters/check.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 main() {
+  biome check --error-on-warnings || return 1
   ruff check || return 1
   ruff format --diff || return 1
   return 0
---
+++ main.js
@@ -0,0 +1,18 @@
+import { Error404 } from "./views/pages/error404.js";
+import { Home } from "./views/pages/home.js";
+import { Login } from "./views/pages/login.js";
+import { Router } from "./core/router.js";
+
+
+async function main() {
+  const routes = {
+    '/': Home,
+    '/login': Login,
+    '/404': Error404,
+  };
+  var router = new Router(routes, document.getElementById("root"))
+  router.route()
+}
+
+document.addEventListener('load', main);
+document.addEventListener("DOMContentLoaded", main)
