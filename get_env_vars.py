497599c1575941531012179032bbd234698eb73c
rikeda-cloud <longsichitian648@gmail.com>
2024-12-25 15:39:16 +0900

    [add] vaultサーバでの環境変数の取得を実現

---
+++ get_env_vars.py
@@ -0,0 +1,29 @@
+import requests
+
+# Vaultの設定
+VAULT_HOST = "http://127.0.0.1:8200"
+VAULT_TOKEN = "root"
+KV_PATH = "env-vars"
+API = "users"
+URL = f"{VAULT_HOST}/v1/{KV_PATH}/{API}"
+
+# ヘッダーにVaultのトークンをセット
+headers = {"X-Vault-Token": VAULT_TOKEN}
+response = requests.get(URL, headers=headers)
+
+# レスポンスのステータスコードを確認
+if response.status_code != 200:
+    print(f"Vaultからのデータ取得に失敗しました: {response.status_code}")
+    print(response.text)
+    exit(1)
+else:
+    data = response.json()
+    
+    # 環境変数のキーと値のペアを抽出
+    if "data" in data:
+        env_vars = data["data"]
+        print("=== 環境変数のリスト ===")
+        for key, value in env_vars.items():
+            print(f"{key}: {value}")
+    else:
+        print("データが見つかりませんでした。")
--- setting-vault.sh
+++ setting-vault.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+# JWTへの署名＆公開鍵の配信エンドポイント作成
 vault secrets enable transit
 vault write -f transit/keys/signing-key type=rsa-2048
 vault read transit/keys/signing-key
-
-# policy.hcl
+## policy.hclをvaultコンテナ内に下記の内容で作成する
 """
 path "transit/keys/signing-key" {
   capabilities = ["read"]
 }
 """
-
-
 vault policy write public-key-policy policy.hcl
 vault token create -policy=public-key-policy
 
+
+# 環境変数配信エンドポイント作成
+vault secrets enable -path=env-vars kv
+## users.secret.jsonをvaultコンテナ内に下記の内容で作成する
+"""
+{
+"MASTER_ID":"0",
+"FLAG": "true"
+}
+"""
+vault kv put env-vars/users @/users.secret.json
 
