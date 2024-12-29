73ec7a49be4dc05a5c84a2c7aa5583ca0a405c4d
tachu <roatyungtatyu@gmail.com>
2024-12-29 19:09:47 +0900

    add: game class

--- learning/tterao/ws/backend/wspong/consumers.py
+++ learning/tterao/ws/backend/wspong/consumers.py
@@ -20,16 +20,19 @@ class GameConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         print(text_data_json)
-        message = text_data_json["message"]
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "game.message", "message": message}
+            # self.room_group_name, {"type": "game.message", "message": message}
+            self.room_group_name,
+            {"type": "game.message"} | text_data_json,
         )
 
     # async_to_sync(self.channel_layer.group_send)の時にしてされたtypeがgame.messageのときにこの関数が呼ばれる
     async def game_message(self, event):
+        print("event", event)
         message = event["message"]
+        timer = event["timer"]
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps(event))
--- learning/tterao/ws/frontend/src/game.js
+++ learning/tterao/ws/frontend/src/game.js
@@ -1,15 +1,36 @@
-const gameState = {
-  message: "",
-  timer: 60
+class Game {
+  constructor() {
+    /** @private */
+    this.message_ = ""
+    /** @private */
+    this.timer_ = 60
+  }
+
+  get message() { return this.message_ }
+  set message(message) { this.message_ = message }
+  get timer() { return this.timer_ }
+  set timer(timer) { this.timer_ = timer }
+  toObj() {
+    return {
+      message: this.message_,
+      timer: this.timer_
+    }
+  }
+  copyObj(obj) {
+    this.message_ = obj.message
+    this.timer_ = obj.timer
+  }
 }
 
+const gameState = new Game()
 const ws = new WebSocket(`ws://127.0.0.1:8000/ws/game`)
 
 function startTimer() {
-  gameState.timer = 60;
+  gameState.timer = 60
+  const timerEle = document.querySelector(".js-timer").innerText = gameState.timer
   const timerInterval = setInterval(() => {
     if (gameState.timer > 0) {
-      gameState.timer -= 1;
+      gameState.timer = gameState.timer - 1
       send();
     } else {
       clearInterval(timerInterval);
@@ -20,17 +41,21 @@ function startTimer() {
 
 async function send() {
   gameState.message = document.querySelector(".js-input").value
-  await ws.send(JSON.stringify(gameState))
+  await ws.send(JSON.stringify(gameState.toObj()))
 }
 
 function main() {
+  startTimer()
   ws.onopen = function(e) {
     console.log("ws connection established")
   }
 
   ws.onmessage = function(e) {
     const data = JSON.parse(e.data)
+    const timerEle = document.querySelector(".js-timer")
     const gameEle = document.querySelector(".js-game")
+    gameState.copyObj(data)
+    timerEle.innerText = data.timer
     gameEle.textContent += data.message
     console.log("onmessage", data)
   }
