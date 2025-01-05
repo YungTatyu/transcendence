ea3edee852bcd8a011fcb8be2dc28be5d02b60bc
Your Name <you@example.com>
2025-01-05 17:20:46 +0900

    omit import

--- auth_service/auth_app/serializers.py
+++ auth_service/auth_app/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from utils.redis_handler import RedisHandler
-import uuid
 import json
 
 class SignupSerializer(serializers.ModelSerializer):
