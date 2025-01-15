2e8294ae40b5da15b285cb48205e94686bd1a148
rikeda-cloud <longsichitian648@gmail.com>
2025-01-15 14:59:28 +0900

    [add] tournament model

---
+++ tournament_service/tournament_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils.timezone import now
+
+
+class Tournament(models.Model):
+    tournament_id = models.AutoField(primary_key=True)
+    start_date = models.DateField(default=now)
+    finish_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return (
+            f"Tournament {self.tournament_id} ({self.start_date} - {self.finish_date})"
+        )
