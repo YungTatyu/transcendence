45699ee6e2be3dc8d3f3917202c13f8db062f290
tachu <roatyungtatyu@gmail.com>
2025-01-16 19:27:27 +0900

    modify: MatchManager: static method to class method

--- game_service/core/match_manager.py
+++ game_service/core/match_manager.py
@@ -13,14 +13,14 @@ class MatchManager:
     すべてのmatchを管理する
     """
 
-    class MatchKeys(Enum):
+    class Keys(Enum):
         KEY_MATCH = "match"
         KEY_GAME_CONTROLLER = "game_contoroller"
 
     __matches = {}
 
-    @staticmethod
-    def create_match(match_id, players):
+    @classmethod
+    def create_match(cls, match_id, players):
         """
         新しいMatchを作成
         """
@@ -28,22 +28,27 @@ class MatchManager:
             raise ValueError(f"Match {match_id} already exists.")
         match = Match(match_id, players)
         MatchManager.__matches[match_id] = {
-            MatchManager.MatchKeys.KEY_MATCH.value: match,
-            MatchManager.MatchKeys.KEY_GAME_CONTROLLER.value: GameContoroller(match_id),
+            MatchManager.Keys.KEY_MATCH.value: match,
+            MatchManager.Keys.KEY_GAME_CONTROLLER.value: GameContoroller(match_id),
         }
         return MatchManager.__matches[match_id]
 
-    @staticmethod
-    def get_match(match_id):
+    @classmethod
+    def get_match(cls, match_id):
         """
         指定されたMatchを取得
         """
         return MatchManager.__matches.get(match_id)
 
-    @staticmethod
-    def remove_match(match_id):
+    @classmethod
+    def remove_match(cls, match_id):
         """
         指定されたMatchを削除
         """
         if match_id in MatchManager.__matches:
             del MatchManager.__matches[match_id]
+
+    @classmethod
+    def delete_all_matches(cls):
+        """すべてのマッチを削除する"""
+        MatchManager.__matches.clear()
