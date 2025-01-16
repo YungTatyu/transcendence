3b80a5bb26a59d3cefb10fa42887072126a1de7d
rikeda-cloud <longsichitian648@gmail.com>
2025-01-16 19:08:55 +0900

    [add] トーナメント開始時刻表示用UIをクラス化

--- tournament_service/static/script.js
+++ tournament_service/static/script.js
@@ -1,34 +1,7 @@
-var loc = window.location;
-var uri = (loc.protocol === 'https:') ? "wss:" : "ws:";
-uri += '//' + window.location.host + loc.pathname + 'tournaments/ws/enter-room';
-const ws = new WebSocket(uri);
+const loc = window.location;
+const wsProtocol = (loc.protocol === 'https:') ? "wss:" : "ws:";
+const tournamentMatchingUrl = `${wsProtocol}//${loc.host}${loc.pathname}tournaments/ws/enter-room`;
 
-const tournamentStartTime = document.getElementById('tournamentStartTime');
-
-ws.onmessage = function(event) {
-	const sentData = JSON.parse(event.data);
-	if (sentData.tournament_id) {
-		console.log(sentData.tournament_id);
-		ws.close();
-	} else if (sentData.tournament_start_time) {
-		if (sentData.tournament_start_time === "None") {
-			tournamentStartTime.style.display = "none";
-		} else {
-			tournamentStartTime.style.display = "block";
-			tournamentStartTime.textContent = calcRemainingTime(sentData.tournament_start_time);
-		}
-		console.log(sentData.tournament_start_time);
-	} else {
-		console.log("Error");
-	}
-};
-
-function calcRemainingTime(tournamentStartTimeString) {
-	const start_time = parseFloat(tournamentStartTimeString);
-	const currentUnixTime = Date.now() / 1000;
-	const remainingTime = Math.round(start_time - currentUnixTime);
-	return remainingTime;
-}
-
-ws.onerror = function(error) { console.error("WebSocket error:", error); };
-ws.onclose = function() { console.log("WebSocket connection closed."); };
+const tournamentStartTimeElement = document.getElementById('tournamentStartTime');
+const tournamentMatchingWsHandler = new TournamentMatchingWsHandler(tournamentMatchingUrl, tournamentStartTimeElement);
+tournamentMatchingWsHandler.connect();
---
+++ tournament_service/static/tournament_matching_ws_handler.js
@@ -0,0 +1,64 @@
+class TournamentMatchingWsHandler {
+	/**
+	 * @param {string} url トーナメントマッチング機能のエンドポイント
+	 * @param {HTMLElement} startTimeElement タイマーを表示するHTML要素
+	 */
+	constructor(url, startTimeElement) {
+		this.webSocketUrl = url;
+		this.startTimeElement = startTimeElement;
+		this.timer = new CountDownTimer(startTimeElement);
+		this.ws = null;
+	}
+
+	// TournamentMatchingへのWebSocket接続を開始する
+	connect() {
+		this.ws = new WebSocket(this.webSocketUrl);
+
+		// WebSocketからのメッセージの受信時
+		this.ws.onmessage = (event) => {
+			this.handleMessage(event);
+		};
+
+		// WebSocketでのエラー発生時
+		this.ws.onerror = (error) => {
+			console.error("WebSocket error:", error);
+			this.timer.clear();
+		};
+
+		// WebSocket切断時
+		this.ws.onclose = () => {
+			console.log("WebSocket connection closed.");
+			// this.timer.clear();
+		};
+	}
+
+	handleMessage(event) {
+		const sentData = JSON.parse(event.data);
+		console.log("recieve", sentData.tournament_id, sentData.tournament_start_time);
+
+		if (sentData.tournament_id) {
+			this.handleStartTournament(sentData.tournament_id);
+		} else if (sentData.tournament_start_time) {
+			this.handleDisplayTournamentStartTime(sentData.tournament_start_time);
+		} else {
+			console.log("Error: Unknown data received.");
+		}
+	}
+
+	handleStartTournament(tournamentId) {
+		console.log(`Tournament ID: ${tournamentId}`);
+		this.timer.clear();
+		// トーナメントマッチングルーム用WebSocketは削除
+		this.ws.close();
+		// TODO トーナメント用WebSocketを作成
+	}
+
+	handleDisplayTournamentStartTime(startTime) {
+		if (startTime === "None") {
+			this.timer.clear();
+		}
+		else {
+			this.timer.start(startTime);
+		}
+	}
+}
