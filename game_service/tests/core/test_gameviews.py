1258eb42258cbac0eb0194ad59edbf2a15d7532c
tachu <roatyungtatyu@gmail.com>
2025-01-20 18:50:12 +0900

    refactor: test_gameviews.py

--- game_service/tests/core/test_gameviews.py
+++ game_service/tests/core/test_gameviews.py
@@ -16,49 +16,52 @@ class GameViewsTestCase(APITestCase):
         return self.client.post(self.url, data, format="json")
 
     def test_create_match_with_two_players(self):
-        data = {GameSerializer.KEY_MATCH_ID: 1, GameSerializer.KEY_USERS: [1, 2]}
-        response = self.client.post(self.url, data, format="json")
+        response = self.create_match(
+            {GameSerializer.KEY_MATCH_ID: 1, GameSerializer.KEY_USERS: [1, 2]}
+        )
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIsNotNone(MatchManager.get_match(1))
 
     def test_create_match_with_one_player(self):
-        data = {GameSerializer.KEY_MATCH_ID: 2, GameSerializer.KEY_USERS: [1]}
-        response = self.client.post(self.url, data, format="json")
+        response = self.create_match(
+            {GameSerializer.KEY_MATCH_ID: 2, GameSerializer.KEY_USERS: [1]}
+        )
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIsNotNone(MatchManager.get_match(2))
 
     def test_create_match_with_three_players(self):
-        data = {GameSerializer.KEY_MATCH_ID: 3, GameSerializer.KEY_USERS: [1, 2, 3]}
-        response = self.client.post(self.url, data, format="json")
+        response = self.create_match(
+            {GameSerializer.KEY_MATCH_ID: 3, GameSerializer.KEY_USERS: [1, 2, 3]}
+        )
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertIsNotNone(MatchManager.get_match(3))
 
     def test_create_match_with_no_players(self):
-        data = {GameSerializer.KEY_MATCH_ID: 1, GameSerializer.KEY_USERS: []}
-        response = self.client.post(self.url, data, format="json")
+        response = self.create_match(
+            {GameSerializer.KEY_MATCH_ID: 1, GameSerializer.KEY_USERS: []}
+        )
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIsNone(MatchManager.get_match(1))
 
     def test_create_match_with_no_matchid(self):
-        data = {GameSerializer.KEY_MATCH_ID: "", GameSerializer.KEY_USERS: [1, 2]}
-        response = self.client.post(self.url, data, format="json")
+        response = self.create_match(
+            {GameSerializer.KEY_MATCH_ID: "", GameSerializer.KEY_USERS: [1, 2]}
+        )
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_match_with_invalid_matchid_key(self):
-        data = {"match_id": 1, GameSerializer.KEY_USERS: [1, 2]}
-        response = self.client.post(self.url, data, format="json")
+        response = self.create_match({"match_id": 1, GameSerializer.KEY_USERS: [1, 2]})
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIsNone(MatchManager.get_match(1))
 
     def test_create_match_with_invalid_userid_list_key(self):
-        data = {GameSerializer.KEY_MATCH_ID: 1, "users": [1, 2]}
-        response = self.client.post(self.url, data, format="json")
+        response = self.create_match({GameSerializer.KEY_MATCH_ID: 1, "users": [1, 2]})
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIsNone(MatchManager.get_match(1))
@@ -79,13 +82,15 @@ class GameViewsTestCase(APITestCase):
         self.assertEqual(match_before, match_after)
 
     def test_create_match_with_negative_matchid(self):
-        data = {GameSerializer.KEY_MATCH_ID: -2, GameSerializer.KEY_USERS: [1, 2]}
-        response = self.client.post(self.url, data, format="json")
+        response = self.create_match(
+            {GameSerializer.KEY_MATCH_ID: -2, GameSerializer.KEY_USERS: [1, 2]}
+        )
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_match_with_negative_userid(self):
-        data = {GameSerializer.KEY_MATCH_ID: 2, GameSerializer.KEY_USERS: [1, -2]}
-        response = self.client.post(self.url, data, format="json")
+        response = self.create_match(
+            {GameSerializer.KEY_MATCH_ID: 2, GameSerializer.KEY_USERS: [1, -2]}
+        )
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
