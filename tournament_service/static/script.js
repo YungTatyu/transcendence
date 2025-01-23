d0bcfcee5405db0654cd52e5edbe6e64e6c488c3
rikeda-cloud <longsichitian648@gmail.com>
2025-01-23 18:21:11 +0900

    [format] *.js

--- tournament_service/static/script.js
+++ tournament_service/static/script.js
@@ -1,7 +1,12 @@
 const loc = window.location;
-const wsProtocol = (loc.protocol === 'https:') ? "wss:" : "ws:";
+const wsProtocol = loc.protocol === "https:" ? "wss:" : "ws:";
 const tournamentMatchingUrl = `${wsProtocol}//${loc.host}${loc.pathname}tournaments/ws/enter-room`;
 
-const tournamentStartTimeElement = document.getElementById('tournamentStartTime');
-const tournamentMatchingWsHandler = new TournamentMatchingWsHandler(tournamentMatchingUrl, tournamentStartTimeElement);
+const tournamentStartTimeElement = document.getElementById(
+  "tournamentStartTime",
+);
+const tournamentMatchingWsHandler = new TournamentMatchingWsHandler(
+  tournamentMatchingUrl,
+  tournamentStartTimeElement,
+);
 tournamentMatchingWsHandler.connect();
