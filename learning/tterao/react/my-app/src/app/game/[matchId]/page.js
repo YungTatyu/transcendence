fbea2d166c0a7b39c6e86a01f113a35cf615b348
tachu <roatyungtatyu@gmail.com>
2025-01-25 15:09:31 +0900

    add: connectToWebSocket

--- learning/tterao/react/my-app/src/app/game/[matchId]/page.js
+++ learning/tterao/react/my-app/src/app/game/[matchId]/page.js
@@ -1,8 +1,33 @@
 'use client'
+import { useUsername } from "@/app/UsernameContext";
 import { useParams } from "next/navigation"
+
+export function connectToWebSocket(matchId) {
+  const REDIS_SERVER = "127.0.0.1:3000"
+  const socket = new WebSocket(`ws://${REDIS_SERVER}/games/ws/enter-room/${matchId}`);
+
+  socket.onopen = () => {
+    console.log(`Connected to match: ${matchId}`);
+  };
+
+  socket.onmessage = (message) => {
+    console.log('Message from server:', message.data);
+  };
+
+  socket.onclose = () => {
+    console.log('Disconnected from server');
+  };
+
+  socket.onerror = () => {
+    console.log('error');
+  };
+
+  return socket;
+}
 
 export default function Game() {
   const { matchId } = useParams();
+  const { userid } = useUsername()
   return <div>
     game
     <div>{matchId}</div>
