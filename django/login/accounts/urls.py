5420c63af2ba081a67b53270c49908776a1c1e2b
kealoke <yuna.kealoke@gmail.com>
2025-01-25 14:32:17 +0900

    Jan 25

--- django/login/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-#ユーザー登録用のURLパターン
-
-urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
-]
--- django/login/chat/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Friends
-
-class FriendsAdmin(admin.ModelAdmin):
-    list_display = ('pk','user', 'friend',)
-
-admin.site.register(Friends, FriendsAdmin)
