9f6ec807aa56af7a52e5a56c5493138609459457
tachu <roatyungtatyu@gmail.com>
2025-01-28 13:12:07 +0900

    add: test routing

--- game_service/realtime_pingpong/routing.py
+++ game_service/realtime_pingpong/routing.py
@@ -5,6 +5,12 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(
         r"games/ws/enter-room/(?P<matchId>\d+)$",  # \d+ で数字のみを許容
-        consumers.ChatConsumer.as_asgi(),
+        consumers.GameConsumer.as_asgi(),
+    ),
+    # TODO このエンドポイントは動作確認用
+    # jwt認証機能が追加されたら削除する
+    re_path(
+        r"games/ws/enter-room/(?P<matchId>\d+)/(?P<userId>[a-zA-Z0-9]+)$",
+        consumers.GameConsumer.as_asgi(),
     ),
 ]
