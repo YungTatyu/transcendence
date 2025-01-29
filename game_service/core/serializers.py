90303092325b3fd50e7cd151930fc4948d1b9841
tachu <roatyungtatyu@gmail.com>
2025-01-29 19:30:17 +0900

    fix: naming rule

--- game_service/core/serializers.py
+++ game_service/core/serializers.py
@@ -7,18 +7,19 @@ class GameSerializer(serializers.Serializer):
     """
     以下の変数名は、requestのkeyの命名と完全に一致している必要がある
     """
-    matchId = serializers.IntegerField(min_value=0, required=True)
-    userIdList = serializers.ListField(
+
+    matchId = serializers.IntegerField(min_value=0, required=True)  # noqa: N815
+    userIdList = serializers.ListField(  # noqa: N815
         child=serializers.IntegerField(min_value=0),
         min_length=1,
         required=True,
     )
 
-    def validate_matchId(self, value):
+    def validate_matchId(self, value):  # noqa: N802
         # TODO: match_idがmatchサービスに存在するか確認必要？
         return value
 
-    def validate_userIdList(self, users):
+    def validate_userIdList(self, users):  # noqa: N802
         # TODO: userが存在するか確認必要？
 
         # userの重複チェック
