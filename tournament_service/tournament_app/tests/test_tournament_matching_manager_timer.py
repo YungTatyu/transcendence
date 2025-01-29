ea6ac71feddcc4c1962ecad5111774a5a99becb0
tachu <roatyungtatyu@gmail.com>
2025-01-29 21:13:14 +0900

    fix: naming rule

--- tournament_service/tournament_app/tests/test_tournament_matching_manager_timer.py
+++ tournament_service/tournament_app/tests/test_tournament_matching_manager_timer.py
@@ -3,7 +3,7 @@ import asyncio
 from django.test import TestCase
 
 from tournament_app.utils.tournament_matching_manager import (
-    TournamentMatchingManager as TMM,
+    TournamentMatchingManager as Tmm,
 )
 
 
@@ -12,28 +12,28 @@ class TournamentMatchingManagerTimerTest(TestCase):
 
     async def __cancel_task_timer(self):
         """タスクとして実行される関数内でタイマーをキャンセル"""
-        TMM.cancel_task()
+        Tmm.cancel_task()
 
     async def test_cancel_task_timer(self):
-        TMM.set_task(self.delay, self.__cancel_task_timer)
-        self.assertIsNotNone(TMM.get_task())
-        TMM.cancel_task()
+        Tmm.set_task(self.delay, self.__cancel_task_timer)
+        self.assertIsNotNone(Tmm.get_task())
+        Tmm.cancel_task()
         await asyncio.sleep(0.1)
-        self.assertIsNone(TMM.get_task())
+        self.assertIsNone(Tmm.get_task())
 
     async def test_cancel_task_timer_in_task(self):
         """
         タスクとして実行される関数内でタスクタイマーをキャンセルした場合
         """
-        TMM.set_task(self.delay, self.__cancel_task_timer)
-        self.assertIsNotNone(TMM.get_task())
+        Tmm.set_task(self.delay, self.__cancel_task_timer)
+        self.assertIsNotNone(Tmm.get_task())
         await asyncio.sleep(self.delay + 1)
-        self.assertIsNone(TMM.get_task())
+        self.assertIsNone(Tmm.get_task())
 
     async def test_task_excution_time(self):
         """タスクが終了したらtask_execution_timeはNoneが返るか"""
-        self.assertIsNone(TMM.get_task_execution_time())
-        TMM.set_task(self.delay, self.__cancel_task_timer)
-        self.assertIsNotNone(TMM.get_task_execution_time())
+        self.assertIsNone(Tmm.get_task_execution_time())
+        Tmm.set_task(self.delay, self.__cancel_task_timer)
+        self.assertIsNotNone(Tmm.get_task_execution_time())
         await asyncio.sleep(self.delay + 1)
-        self.assertIsNone(TMM.get_task_execution_time())
+        self.assertIsNone(Tmm.get_task_execution_time())
