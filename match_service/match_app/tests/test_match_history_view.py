03f9b9f5188675174906b232b5f6b02a5b73e2dc
rikeda-cloud <longsichitian648@gmail.com>
2025-01-30 18:59:03 +0900

    [fix] MatchHistoryViewでlimitが100を超える場合のTestCaseを追加&refactor

--- match_service/match_app/tests/test_match_history_view.py
+++ match_service/match_app/tests/test_match_history_view.py
@@ -3,22 +3,41 @@ from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from .set_up_utils import create_query_string
 from .set_up_records import set_up_records
 
+# INFO docs/matches.ymlで/matches/histories/エンドポイントのlimitのdefault値は10と定義しています
+DEFAULT_LIMIT = 10
 
-def request_match_histories(client, status, user_id, offset=None, limit=None) -> dict:
+# INFO docs/matches.ymlで/matches/histories/エンドポイントのlimitのmax値は100と定義しています
+MAX_LIMIT = 100
+
+# INFO docs/matches.ymlで/matches/histories/エンドポイントのoffsetのdefault値は0と定義しています
+DEFAULT_OFFSET = 0
+
+
+def request_match_histories(
+    client, status, user_id, expect_total, expect_limit, offset=None, limit=None
+) -> dict:
     query_string = create_query_string(offset=offset, limit=limit)
     response = client.get(f"/matches/histories/{user_id}/{query_string}")
     assert response.status_code == status
+
+    if response.status_code == HTTP_200_OK:
+        res_data = response.json()
+        assert res_data["total"] == expect_total
+        expect_offset = DEFAULT_OFFSET if offset is None else offset
+        assert res_data["offset"] == expect_offset
+        assert res_data["limit"] == expect_limit
+
     return response.json()
 
 
 @pytest.mark.django_db
 def test_user1(client, set_up_records):
     user1_id = 1
-    res_data = request_match_histories(client, HTTP_200_OK, user1_id)
-
-    assert res_data["total"] == 2
-    assert res_data["offset"] == 0
-    assert res_data["limit"] == 2
+    expect_total = 2
+    expect_limit = min(expect_total, DEFAULT_LIMIT)
+    res_data = request_match_histories(
+        client, HTTP_200_OK, user1_id, expect_total, expect_limit
+    )
 
     for result in res_data["results"]:
         assert result["mode"] == "Tournament"
@@ -31,11 +50,11 @@ def test_user1(client, set_up_records):
 @pytest.mark.django_db
 def test_user2(client, set_up_records):
     user2_id = 2
-    res_data = request_match_histories(client, HTTP_200_OK, user2_id)
-
-    assert res_data["total"] == 2
-    assert res_data["offset"] == 0
-    assert res_data["limit"] == 2
+    expect_total = 2
+    expect_limit = min(expect_total, DEFAULT_LIMIT)
+    res_data = request_match_histories(
+        client, HTTP_200_OK, user2_id, expect_total, expect_limit
+    )
 
     for result in res_data["results"]:
         assert result["mode"] == "Tournament"
@@ -48,11 +67,11 @@ def test_user2(client, set_up_records):
 @pytest.mark.django_db
 def test_user3(client, set_up_records):
     user3_id = 3
-    res_data = request_match_histories(client, HTTP_200_OK, user3_id)
-
-    assert res_data["total"] == 1
-    assert res_data["offset"] == 0
-    assert res_data["limit"] == 1
+    expect_total = 1
+    expect_limit = min(expect_total, DEFAULT_LIMIT)
+    res_data = request_match_histories(
+        client, HTTP_200_OK, user3_id, expect_total, expect_limit
+    )
 
     for result in res_data["results"]:
         assert result["mode"] == "QuickPlay"
@@ -65,11 +84,11 @@ def test_user3(client, set_up_records):
 @pytest.mark.django_db
 def test_user4(client, set_up_records):
     user4_id = 4
-    res_data = request_match_histories(client, HTTP_200_OK, user4_id)
-
-    assert res_data["total"] == 1
-    assert res_data["offset"] == 0
-    assert res_data["limit"] == 1
+    expect_total = 1
+    expect_limit = min(expect_total, DEFAULT_LIMIT)
+    res_data = request_match_histories(
+        client, HTTP_200_OK, user4_id, expect_total, expect_limit
+    )
 
     for result in res_data["results"]:
         assert result["mode"] == "QuickPlay"
@@ -83,11 +102,11 @@ def test_user4(client, set_up_records):
 def test_user_not_exist(client, set_up_records):
     """存在しないユーザーでも正常にレスポンスが返る"""
     not_exist_user_id = 12345
-    res_data = request_match_histories(client, HTTP_200_OK, not_exist_user_id)
-
-    assert res_data["total"] == 0
-    assert res_data["offset"] == 0
-    assert res_data["limit"] == 0
+    expect_total = 0
+    expect_limit = min(expect_total, DEFAULT_LIMIT)
+    res_data = request_match_histories(
+        client, HTTP_200_OK, not_exist_user_id, expect_total, expect_limit
+    )
     assert res_data["results"] == []
 
 
@@ -95,7 +114,7 @@ def test_user_not_exist(client, set_up_records):
 def test_user_id_is_not_digit(client, set_up_records):
     """URLのPathのuser_idが数値でないなら400が返る"""
     not_digit_user_id = "abcde"
-    request_match_histories(client, HTTP_400_BAD_REQUEST, not_digit_user_id)
+    request_match_histories(client, HTTP_400_BAD_REQUEST, not_digit_user_id, None, None)
 
 
 @pytest.mark.django_db
@@ -106,10 +125,11 @@ def test_offset(client, set_up_records):
     """
     user1_id = 1
     offset = 1
-    res_data = request_match_histories(client, HTTP_200_OK, user1_id, offset=offset)
-    assert res_data["total"] == 2
-    assert res_data["offset"] == 1
-    assert res_data["limit"] == 1
+    expect_total = 2
+    expect_limit = min(expect_total - offset, DEFAULT_LIMIT)
+    res_data = request_match_histories(
+        client, HTTP_200_OK, user1_id, expect_total, expect_limit, offset=offset
+    )
 
     for result in res_data["results"]:
         assert result["mode"] == "Tournament"
@@ -127,10 +147,11 @@ def test_limit(client, set_up_records):
     """
     user1_id = 1
     limit = 1
-    res_data = request_match_histories(client, HTTP_200_OK, user1_id, limit=limit)
-    assert res_data["total"] == 2
-    assert res_data["offset"] == 0
-    assert res_data["limit"] == 1
+    expect_total = 2
+    expect_limit = min(expect_total, limit)
+    res_data = request_match_histories(
+        client, HTTP_200_OK, user1_id, expect_total, expect_limit, limit=limit
+    )
 
     result = res_data["results"][0]
     assert result["mode"] == "Tournament"
@@ -148,10 +169,11 @@ def test_offset_over_total(client, set_up_records):
     """
     user1_id = 1
     offset = 3
-    res_data = request_match_histories(client, HTTP_200_OK, user1_id, offset=offset)
-    assert res_data["total"] == 2
-    assert res_data["offset"] == 3
-    assert res_data["limit"] == 0
+    expect_total = 2
+    expect_limit = max(expect_total - offset, 0)
+    res_data = request_match_histories(
+        client, HTTP_200_OK, user1_id, expect_total, expect_limit, offset=offset
+    )
     assert res_data["results"] == []
 
 
@@ -164,10 +186,11 @@ def test_limit_over_total(client, set_up_records):
     """
     user1_id = 1
     limit = 3
-    res_data = request_match_histories(client, HTTP_200_OK, user1_id, limit=limit)
-    assert res_data["total"] == 2
-    assert res_data["offset"] == 0
-    assert res_data["limit"] == 2
+    expect_total = 2
+    expect_limit = min(expect_total, limit)
+    res_data = request_match_histories(
+        client, HTTP_200_OK, user1_id, expect_total, expect_limit, limit=limit
+    )
 
     for result in res_data["results"]:
         assert result["mode"] == "Tournament"
@@ -185,12 +208,17 @@ def test_offset_and_limit(client, set_up_records):
     user1_id = 1
     offset = 1
     limit = 1
+    expect_total = 2
+    expect_limit = min(expect_total - offset, limit)
     res_data = request_match_histories(
-        client, HTTP_200_OK, user1_id, offset=offset, limit=limit
+        client,
+        HTTP_200_OK,
+        user1_id,
+        expect_total,
+        expect_limit,
+        offset=offset,
+        limit=limit,
     )
-    assert res_data["total"] == 2
-    assert res_data["offset"] == 1
-    assert res_data["limit"] == 1
 
     result = res_data["results"][0]
     assert result["mode"] == "Tournament"
@@ -209,12 +237,17 @@ def test_offset_and_limit_over_total(client, set_up_records):
     user1_id = 1
     offset = 1
     limit = 10
+    expect_total = 2
+    expect_limit = min(expect_total - offset, limit)
     res_data = request_match_histories(
-        client, HTTP_200_OK, user1_id, offset=offset, limit=limit
+        client,
+        HTTP_200_OK,
+        user1_id,
+        expect_total,
+        expect_limit,
+        offset=offset,
+        limit=limit,
     )
-    assert res_data["total"] == 2
-    assert res_data["offset"] == 1
-    assert res_data["limit"] == 1
 
     result = res_data["results"][0]
     assert result["mode"] == "Tournament"
@@ -222,3 +255,13 @@ def test_offset_and_limit_over_total(client, set_up_records):
     assert result["date"] is not None
     assert result["userScore"] == 11
     assert result["opponents"][0] == {"id": 2, "score": 0}
+
+
+@pytest.mark.django_db
+def test_limit_over_max_limit(client, set_up_records):
+    """limitに指定可能な値よりも大きい値の場合、エラー"""
+    user_id = 1
+    limit = MAX_LIMIT + 1
+    request_match_histories(
+        client, HTTP_400_BAD_REQUEST, user_id, None, None, limit=limit
+    )
