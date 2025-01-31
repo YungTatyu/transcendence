2a4a15b9f34090c5ebeb5791488c9d0c92c52b8c
Your Name <you@example.com>
2025-01-31 22:38:08 +0900

    fix dir views

--- auth_service/auth_app/urls.py
+++ auth_service/auth_app/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.urls import path
 
-from auth_app.views import OTPVerificationView, SignupView
+from auth_app.views.signup_views import OTPVerificationView, SignupView
 from auth_app.views.auth_views import UpdateEmailView
 
 urlpatterns = [
