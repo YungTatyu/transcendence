43b18e60f437197147e3e4de0d6f1fcdaa341cef
Your Name <you@example.com>
2025-02-02 13:08:06 +0900

    add login and login verify endpoint

--- auth_service/auth_app/urls.py
+++ auth_service/auth_app/urls.py
@@ -21,11 +21,14 @@ from auth_app.views.signup_views import OTPVerificationView, SignupView
 from auth_app.views.update_email_view import UpdateEmailView
 from auth_app.views.update_password_view import UpdatePasswordView
 from auth_app.views.health_check_view import HealthCheckView
+from auth_app.views.otp_login_view import OTPLoginView, OTPVerificationView
 
 urlpatterns = [
     path("auth/otp/signup", SignupView.as_view(), name="otp-signup"),
-    path("auth/otp/signup/verify", OTPVerificationView.as_view(), name="otp-verify"),
+    path("auth/otp/signup/verify", OTPVerificationView.as_view(), name="otp-signup-verify"),
     path("auth/me/email", UpdateEmailView.as_view(), name="update_email"),
     path("auth/me/password", UpdatePasswordView.as_view(), name="update_password"),
+    path("auth/otp/login/", OTPLoginView.as_view(), name="otp-login"),
+    path("auth/otp/login/verify", OTPVerificationView.as_view(), name="otp-login-verify"),
     path("health", HealthCheckView.as_view(), name="health_check"),
 ]
