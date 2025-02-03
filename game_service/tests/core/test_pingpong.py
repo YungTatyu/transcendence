49408421e277cf94ce4a67e3127f148141bbfef8
tachu <roatyungtatyu@gmail.com>
2025-02-03 15:16:57 +0900

    add: hit paddle tests

--- game_service/tests/core/test_pingpong.py
+++ game_service/tests/core/test_pingpong.py
@@ -42,3 +42,34 @@ class TestBall(unittest.TestCase):
         old_speed = self.ball.y_speed
         self.ball.hit_wall()
         self.assertEqual(self.ball.y_speed, old_speed)
+
+    def test_hit_left_paddle(self):
+        self.ball.x_pos = Screen.LEFTEST_POS.value
+        self.ball.y_pos = self.left_player.paddle.y_pos + (
+            self.left_player.paddle.HEIGHT / 2
+        )
+        old_speed = self.ball.x_speed
+        self.ball.hit_paddle(self.left_player.paddle, self.right_player.paddle)
+        self.assertEqual(self.ball.x_speed, -old_speed * self.ball.ACCELERATION)
+
+    def test_hit_left_paddle_edge_top(self):
+        """ボールが左パドルの上端に当たるケース"""
+        self.ball.x_pos = Screen.LEFTEST_POS.value
+        # パドルの上端ぎりぎり
+        self.ball.y_pos = self.left_player.paddle.y_pos - self.ball.HEIGHT + 1
+        old_speed = self.ball.x_speed
+
+        self.ball.hit_paddle(self.left_player.paddle, self.right_player.paddle)
+        self.assertEqual(self.ball.x_speed, -old_speed * self.ball.ACCELERATION)
+
+    def test_hit_left_paddle_edge_bottom(self):
+        """ボールが左パドルの下端に当たるケース"""
+        self.ball.x_pos = Screen.LEFTEST_POS.value  # ボールを左端に配置
+        # パドルの下端ぎりぎり
+        self.ball.y_pos = (
+            self.left_player.paddle.y_pos + self.left_player.paddle.HEIGHT - 1
+        )
+        old_speed = self.ball.x_speed
+
+        self.ball.hit_paddle(self.left_player.paddle, self.right_player.paddle)
+        self.assertEqual(self.ball.x_speed, -old_speed * self.ball.ACCELERATION)
