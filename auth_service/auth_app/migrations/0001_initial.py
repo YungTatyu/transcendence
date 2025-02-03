0d4f322290ad9fa783c76313df74c47506af84af
Your Name <you@example.com>
2025-02-03 20:15:21 +0900

    fix lint

--- auth_service/auth_app/migrations/0001_initial.py
+++ auth_service/auth_app/migrations/0001_initial.py
@@ -4,25 +4,43 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='CustomUser',
+            name="CustomUser",
             fields=[
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('user_id', models.CharField(max_length=36, primary_key=True, serialize=False, unique=True)),
-                ('email', models.EmailField(max_length=255, unique=True, verbose_name='Email Address')),
-                ('secret_key', models.CharField(blank=True, max_length=128, null=True, verbose_name='Secret Key')),
-                ('date_joined', models.DateTimeField(auto_now_add=True)),
+                ("password", models.CharField(max_length=128, verbose_name="password")),
+                (
+                    "last_login",
+                    models.DateTimeField(
+                        blank=True, null=True, verbose_name="last login"
+                    ),
+                ),
+                (
+                    "user_id",
+                    models.CharField(
+                        max_length=36, primary_key=True, serialize=False, unique=True
+                    ),
+                ),
+                (
+                    "email",
+                    models.EmailField(
+                        max_length=255, unique=True, verbose_name="Email Address"
+                    ),
+                ),
+                (
+                    "secret_key",
+                    models.CharField(
+                        blank=True, max_length=128, null=True, verbose_name="Secret Key"
+                    ),
+                ),
+                ("date_joined", models.DateTimeField(auto_now_add=True)),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
     ]
