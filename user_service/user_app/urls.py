30922dfd86cd56ddd56ad3b91c7fad360406cba7
kealoke <yuna.kealoke@gmail.com>
2025-02-03 18:44:52 +0900

    [change] name to user_app

--- user_service/user_app/urls.py
+++ user_service/user_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for user_service project.
+URL configuration for user_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
