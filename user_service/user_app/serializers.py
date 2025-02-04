228ebb1418f59332794ec71d9e696c59252d37b0
kealoke <yuna.kealoke@gmail.com>
2025-02-04 20:41:19 +0900

    [add] user get method

--- user_service/user_app/serializers.py
+++ user_service/user_app/serializers.py
@@ -5,3 +5,7 @@ from .models import User
 class createUserSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=10)
 
+class searchUserSerializer(serializers.Serializer):
+    userId = serializers.IntegerField()
+    username = serializers.CharField(max_length=10)
+    avatarPath = serializers.CharField(max_length=100)
--- user_service/user_app/views.py
+++ user_service/user_app/views.py
@@ -1,10 +1,10 @@
-from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_201_CREATED
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT, HTTP_201_CREATED, HTTP_404_NOT_FOUND, HTTP_200_OK
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models  import User
-from .serializers import createUserSerializer
+from .serializers import createUserSerializer, searchUserSerializer
 
 class UserView(APIView):
     def post(self, request):
@@ -22,6 +22,28 @@ class UserView(APIView):
         data = {"userId": user.user_id, "username": user.name}
 
         return Response(data, status=HTTP_201_CREATED)
+    
+    def get(self, request):
+        username = request.GET.get("username")
+        userid = request.GET.get("userid")
+
+        if not username and not userid:
+            return Response({"error": "query parameter 'username' or 'userid' is required."}, status=HTTP_400_BAD_REQUEST)
+        
+        if username and userid:
+            return Response({"error": "query parameter 'username' or 'userid' is required."}, status=HTTP_400_BAD_REQUEST)
+        
+        if username:
+            user = User.objects.filter(name=username).first()
+        elif userid:
+            user = User.objects.filter(user_id=userid).first()
+
+        if not user:
+            return Response({"error": "User not found."}, status=HTTP_404_NOT_FOUND)
+        
+        serializer = searchUserSerializer(user)
+        return Response(serializer.data, status=HTTP_200_OK)
+    
 
 
 @api_view(["GET"])
