35a99684f08bece3e3593f9f447ed01a0b6be1b2
rikeda-cloud <longsichitian648@gmail.com>
2025-02-07 19:25:02 +0900

    [refactor] views.pyに記述していたViewクラスをviewsディレクトリ内に分割

--- match_service/match_app/urls.py
+++ match_service/match_app/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from .views import (
-    MatchHistoryView,
-    MatchView,
-    health_check,
-)
+from match_app.views.health_check import health_check
+from match_app.views.match_view import MatchView
+from match_app.views.match_history_view import MatchHistoryView
 
 urlpatterns = [
     path("matches/", MatchView.as_view(), name="matches"),
