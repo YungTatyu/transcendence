4ac74356fc8a4b74fed7566de66643fcdf2568dc
rikeda-cloud <longsichitian648@gmail.com>
2025-02-08 23:00:11 +0900

    [refactor] @pytest.mark.parametrizeを用いて似た処理をグループでテストするように変更

--- match_service/match_app/tests/test_match_history_view.py
+++ match_service/match_app/tests/test_match_history_view.py
@@ -36,52 +36,60 @@ class TestMatchHistory:
         assert result["opponents"][0] == opponents
 
     @pytest.mark.django_db
-    def test_user1(self, client, set_up_records):
-        user1_id = 1
+    @pytest.mark.parametrize(
+        "user_id, expect_total, mode, win_or_lose, user_score, opponent_data",
+        [
+            (1, 2, "Tournament", "win", 11, {"id": 2, "score": 0}),
+            (2, 2, "Tournament", "lose", 0, {"id": 1, "score": 11}),
+            (3, 1, "QuickPlay", "win", 11, {"id": 4, "score": 5}),
+            (4, 1, "QuickPlay", "lose", 5, {"id": 3, "score": 11}),
+        ],
+    )
+    def test_userx(
+        self,
+        client,
+        set_up_records,
+        user_id,
+        expect_total,
+        mode,
+        win_or_lose,
+        user_score,
+        opponent_data,
+    ):
+        expect_limit = min(expect_total, MatchHistorySerializer.DEFAULT_LIMIT)
+        res_data = self.request_match_histories(
+            client, HTTP_200_OK, user_id, expect_total, expect_limit
+        )
+
+        for result in res_data["results"]:
+            self.assert_result(result, mode, win_or_lose, user_score, opponent_data)
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize(
+        "expect_limit, offset, limit",
+        [
+            (1, 1, None),  # ?offset=1
+            (1, None, 1),  # ?limit=1
+            (0, 3, None),  # ?offset=3(offset over total)
+            (2, None, 3),  # ?limit=3(limit over total)
+            (1, 1, 1),  # ?offset=1&limit=1
+            (1, 1, 10),  # ?offset=1&limit=10(limit over total)
+        ],
+    )
+    def test_offset_and_limit(
+        self, client, set_up_records, expect_limit, offset, limit
+    ):
+        user_id = 1
         expect_total = 2
-        expect_limit = min(expect_total, MatchHistorySerializer.DEFAULT_LIMIT)
-        res_data = self.request_match_histories(
-            client, HTTP_200_OK, user1_id, expect_total, expect_limit
+        self.request_match_histories(
+            client,
+            HTTP_200_OK,
+            user_id,
+            expect_total,
+            expect_limit,
+            offset=offset,
+            limit=limit,
         )
-
-        for result in res_data["results"]:
-            self.assert_result(result, "Tournament", "win", 11, {"id": 2, "score": 0})
-
-    @pytest.mark.django_db
-    def test_user2(self, client, set_up_records):
-        user2_id = 2
-        expect_total = 2
-        expect_limit = min(expect_total, MatchHistorySerializer.DEFAULT_LIMIT)
-        res_data = self.request_match_histories(
-            client, HTTP_200_OK, user2_id, expect_total, expect_limit
-        )
-
-        for result in res_data["results"]:
-            self.assert_result(result, "Tournament", "lose", 0, {"id": 1, "score": 11})
-
-    @pytest.mark.django_db
-    def test_user3(self, client, set_up_records):
-        user3_id = 3
-        expect_total = 1
-        expect_limit = min(expect_total, MatchHistorySerializer.DEFAULT_LIMIT)
-        res_data = self.request_match_histories(
-            client, HTTP_200_OK, user3_id, expect_total, expect_limit
-        )
-
-        for result in res_data["results"]:
-            self.assert_result(result, "QuickPlay", "win", 11, {"id": 4, "score": 5})
-
-    @pytest.mark.django_db
-    def test_user4(self, client, set_up_records):
-        user4_id = 4
-        expect_total = 1
-        expect_limit = min(expect_total, MatchHistorySerializer.DEFAULT_LIMIT)
-        res_data = self.request_match_histories(
-            client, HTTP_200_OK, user4_id, expect_total, expect_limit
-        )
-
-        for result in res_data["results"]:
-            self.assert_result(result, "QuickPlay", "lose", 5, {"id": 3, "score": 11})
 
     @pytest.mark.django_db
     def test_user_not_exist(self, client, set_up_records):
@@ -101,117 +109,3 @@ class TestMatchHistory:
         self.request_match_histories(
             client, HTTP_400_BAD_REQUEST, not_digit_user_id, None, None
         )
-
-    @pytest.mark.django_db
-    def test_offset(self, client, set_up_records):
-        """
-        test_user1に?offset=1を追加
-        offsetが1ずれる分、limit(取得できたレコード数)は1減った値となる
-        """
-        user1_id = 1
-        offset = 1
-        expect_total = 2
-        expect_limit = min(expect_total - offset, MatchHistorySerializer.DEFAULT_LIMIT)
-        res_data = self.request_match_histories(
-            client, HTTP_200_OK, user1_id, expect_total, expect_limit, offset=offset
-        )
-
-        for result in res_data["results"]:
-            self.assert_result(result, "Tournament", "win", 11, {"id": 2, "score": 0})
-
-    @pytest.mark.django_db
-    def test_limit(self, client, set_up_records):
-        """
-        test_user1に?limit=1を追加
-        test_user1では2レコード取得できたが、limit=1なので最大でも1件しか取得できない
-        """
-        user1_id = 1
-        limit = 1
-        expect_total = 2
-        expect_limit = min(expect_total, limit)
-        res_data = self.request_match_histories(
-            client, HTTP_200_OK, user1_id, expect_total, expect_limit, limit=limit
-        )
-
-        result = res_data["results"][0]
-        self.assert_result(result, "Tournament", "win", 11, {"id": 2, "score": 0})
-
-    @pytest.mark.django_db
-    def test_offset_over_total(self, client, set_up_records):
-        """
-        offsetがサーバ内の合計レコード数より大きいケース
-        resultsは空のリストが返る
-        """
-        user1_id = 1
-        offset = 3
-        expect_total = 2
-        expect_limit = max(expect_total - offset, 0)
-        res_data = self.request_match_histories(
-            client, HTTP_200_OK, user1_id, expect_total, expect_limit, offset=offset
-        )
-        assert res_data["results"] == []
-
-    @pytest.mark.django_db
-    def test_limit_over_total(self, client, set_up_records):
-        """
-        limitがサーバ内の合計レコード数より大きいケース
-        レスポンスボディのlimitは
-        QueryStringで指定したlimitに関係なく、取得できたレコード数が返る
-        """
-        user1_id = 1
-        limit = 3
-        expect_total = 2
-        expect_limit = min(expect_total, limit)
-        res_data = self.request_match_histories(
-            client, HTTP_200_OK, user1_id, expect_total, expect_limit, limit=limit
-        )
-
-        for result in res_data["results"]:
-            self.assert_result(result, "Tournament", "win", 11, {"id": 2, "score": 0})
-
-    @pytest.mark.django_db
-    def test_offset_and_limit(self, client, set_up_records):
-        """
-        offsetとlimitのどちらも指定するケース
-        """
-        user1_id = 1
-        offset = 1
-        limit = 1
-        expect_total = 2
-        expect_limit = min(expect_total - offset, limit)
-        res_data = self.request_match_histories(
-            client,
-            HTTP_200_OK,
-            user1_id,
-            expect_total,
-            expect_limit,
-            offset=offset,
-            limit=limit,
-        )
-
-        result = res_data["results"][0]
-        self.assert_result(result, "Tournament", "win", 11, {"id": 2, "score": 0})
-
-    @pytest.mark.django_db
-    def test_offset_and_limit_over_total(self, client, set_up_records):
-        """
-        offsetとlimitのどちらも指定
-        サーバ内のレコード数よりも大きい値が指定されるケース
-        """
-        user1_id = 1
-        offset = 1
-        limit = 10
-        expect_total = 2
-        expect_limit = min(expect_total - offset, limit)
-        res_data = self.request_match_histories(
-            client,
-            HTTP_200_OK,
-            user1_id,
-            expect_total,
-            expect_limit,
-            offset=offset,
-            limit=limit,
-        )
-
-        result = res_data["results"][0]
-        self.assert_result(result, "Tournament", "win", 11, {"id": 2, "score": 0})
