0d35a7cc1d326a6095a20f0f3d8f221ddb841a49
tachu <roatyungtatyu@gmail.com>
2025-02-12 15:41:36 +0900

    fix: JSTTIME

--- game_service/game_app/settings.py
+++ game_service/game_app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from datetime import datetime, timezone, timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,6 +145,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+TIME_ZONE = "Asia/Tokyo"
+USE_TZ = True
 
 LOGGING = {
     "version": 1,
