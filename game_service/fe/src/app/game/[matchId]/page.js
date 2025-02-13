ca014e98ed900fc87f872ef177943a6d31633902
tachu <roatyungtatyu@gmail.com>
2025-02-13 13:44:35 +0900

    modify: show results

--- game_service/fe/src/app/game/[matchId]/page.js
+++ game_service/fe/src/app/game/[matchId]/page.js
@@ -81,6 +81,14 @@ export default function Game() {
             setRemainingTime(calcRemaingTime(endTimeRef.current));
             startTimer();
           }
+          if (
+            parsedMessage.type === "game.finish.message" &&
+            parsedMessage.message === "gameover"
+          ) {
+            console.log(parsedMessage)
+            const results = parsedMessage.results;
+            alert("game over:\n" + results.map(r => `User ${r.userId}: ${r.score}`).join("\n"));
+          }
         } catch (error) {
           console.error("Failed to parse WebSocket message:", error);
         }
