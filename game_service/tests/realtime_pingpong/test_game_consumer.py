a3a218e8f042e04a94286ef84671cdcbc691fa3c
tachu <roatyungtatyu@gmail.com>
2025-02-13 18:18:23 +0900

    refactor

--- game_service/tests/realtime_pingpong/test_game_consumer.py
+++ game_service/tests/realtime_pingpong/test_game_consumer.py
@@ -6,24 +6,18 @@ from realtime_pingpong.consumers import GameConsumer
 
 
 class TestGameConsumer(unittest.IsolatedAsyncioTestCase):
-    def setUp(self):
+    @patch("channels.layers.get_channel_layer", return_value=MagicMock())
+    async def test_finish_game(self, mock_get_channel_layer):
         MatchManager.remove_match = MagicMock(return_value=None)
-        self.patcher = patch(
-            "channels.layers.get_channel_layer", return_value=MagicMock()
+
+        await GameConsumer.finish_game(
+            {
+                "matchId": 1,
+                "results": [
+                    {"userId": 1, "score": 2},
+                    {"userId": 2, "score": 6},
+                ],
+            },
+            "1",
         )
-        self.mock_get_channel_layer = self.patcher.start()
-        self.mock_channel_layer = self.mock_get_channel_layer.return_value
-        self.result = {
-            "matchId": 1,
-            "results": [
-                {"userId": 1, "score": 2},
-                {"userId": 2, "score": 6},
-            ],
-        }
-
-    def tearDown(self):
-        self.patcher.stop()
-
-    async def test_finish_game(self):
-        await GameConsumer.finish_game(self.result, "1")
         MatchManager.remove_match.assert_called_once_with(1)
