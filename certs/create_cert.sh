bb240aa80184436aa8ea7f43663e49f8384703b9
rikeda-cloud <longsichitian648@gmail.com>
2025-02-14 15:55:41 +0900

    [update] certファイルが存在する場合、再度certファイルは生成しないように変更

--- certs/create_cert.sh
+++ certs/create_cert.sh
@@ -19,12 +19,16 @@ SERVER_CONFIG="${SCRIPT_DIR}/server_openssl.cnf"
 KEY_LENGTH="2048"
 VALIDITY_DAYS="365"
 
+# 鍵と証明書がある場合はスクリプトを終了
+if [[ -f "${CA_KEY}" && -f "${CA_CRT}" && -f "${CLIENT_KEY}" && -f "${CLIENT_CRT}" && -f "${SERVER_KEY}" && -f "${SERVER_CRT}" ]]; then
+	echo "secret key and certificate already exist."
+	exit 0
+fi
+
 # 秘密鍵の作成
 openssl genrsa -out ${CA_KEY} ${KEY_LENGTH}
 openssl genrsa -out ${CLIENT_KEY} ${KEY_LENGTH}
 openssl genrsa -out ${SERVER_KEY} ${KEY_LENGTH}
-
-
 
 # 証明書署名要求の作成
 openssl req -new -key ${CLIENT_KEY} -out ${CLIENT_CSR} -config ${CLIENT_CONFIG} -sha256
