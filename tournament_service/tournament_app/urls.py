5500cdcd53e9426370b4f2c4d653b54630fad3d9
rikeda-cloud <longsichitian648@gmail.com>
2025-02-15 14:35:20 +0900

    [add] TournamentMatchFinishView

--- tournament_service/tournament_app/urls.py
+++ tournament_service/tournament_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import health_check
+from .views import health_check, TournamentMatchFinishView
 
 urlpatterns = [
     path("health", health_check, name="health"),
+    path(
+        "tournaments/finish-match",
+        TournamentMatchFinishView.as_view(),
+        name="finish_match",
+    ),
 ]
