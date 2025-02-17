ef4860cfb78929ea7734f0936c5fe570a4df654e
rikeda-cloud <longsichitian648@gmail.com>
2025-02-17 16:34:15 +0900

    [add] TestCase test_tournament_tree

--- tournament_service/tournament_app/tests/test_tournament_tree.py
+++ tournament_service/tournament_app/tests/test_tournament_tree.py
@@ -5,6 +5,77 @@ from tournament_app.utils.tournament_tree import TournamentTree
 
 class TestTournamentTree:
     @pytest.mark.parametrize(
+        "lst, group_size, expect_dict_list",
+        [
+            (
+                [1, 2],
+                2,
+                [{"value_list": [1, 2], "round": 1}],
+            ),
+            (
+                [1, 2, 3],
+                2,
+                [{"value_list": [2], "round": 2}, {"value_list": [1, 3], "round": 1}],
+            ),
+            (
+                [1, 2, 3, 4],
+                2,
+                [
+                    {"value_list": [], "round": 3},
+                    {"value_list": [1, 3], "round": 1},
+                    {"value_list": [2, 4], "round": 2},
+                ],
+            ),
+            (
+                [1, 2, 3, 4, 5],
+                2,
+                [
+                    {"value_list": [], "round": 4},
+                    {"value_list": [2], "round": 2},
+                    {"value_list": [3, 4], "round": 3},
+                    {"value_list": [1, 5], "round": 1},
+                ],
+            ),
+            (
+                [1, 2, 3, 4],
+                3,
+                [
+                    {"value_list": [2, 3], "round": 2},
+                    {"value_list": [1, 4], "round": 1},
+                ],
+            ),
+            (
+                [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                3,
+                [
+                    {"value_list": [], "round": 4},
+                    {"value_list": [1, 4, 7], "round": 1},
+                    {"value_list": [2, 5, 8], "round": 2},
+                    {"value_list": [3, 6, 9], "round": 3},
+                ],
+            ),
+            (
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                3,
+                [
+                    {"value_list": [], "round": 5},
+                    {"value_list": [2, 3], "round": 2},
+                    {"value_list": [4, 5, 6], "round": 3},
+                    {"value_list": [7, 8, 9], "round": 4},
+                    {"value_list": [1, 10], "round": 1},
+                ],
+            ),
+        ],
+    )
+    def test_tournament_tree(self, lst, group_size, expect_dict_list):
+        tree = TournamentTree(lst, group_size)
+        root = tree.root
+
+        for expect_dict, node in zip(expect_dict_list, tree.bfs_iterator(root)):
+            assert expect_dict["value_list"] == node.value_list
+            assert expect_dict["round"] == node.round
+
+    @pytest.mark.parametrize(
         "lst, group_size",
         [
             ([0], 2),  # len(lst) == 0 なのでエラー
