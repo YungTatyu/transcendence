4582933f6eca634ee41d7e077b2344482ed23814
tk1225 <takuxoka1225@gmail.com>
2025-02-22 15:12:04 +0900

    use group_send

--- friends_activity_service/friends_activity_app/consumers.py
+++ friends_activity_service/friends_activity_app/consumers.py
@@ -1,58 +1,59 @@
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.layers import get_channel_layer
 
 
 class LoggedInUsersConsumer(AsyncWebsocketConsumer):
     # ユーザーリストをメモリ内で管理
     user_list = []
+    group_name = "logged_in_users"
 
     async def connect(self):
-        self.channel_name = self.channel_name  # チャネル名の一意性を保つ
         self.user_id = self.scope.get("user_id")
         if not self.user_id:
             await self.close()
             return
 
-        # WebSocket接続の確立
         await self.accept()
-
-        # ユーザーをリストに追加
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.add_user_to_list()
 
     async def disconnect(self, close_code):
-        # WebSocket接続が切断されたときにユーザーをリストから削除
         await self.remove_user_from_list()
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data):
         pass
 
     async def add_user_to_list(self):
-        # ユーザーリストに追加
-        self.user_list.append(self.user_id)
-
-        # ユーザーリストをクライアントに送信
-        await self.send(
-            text_data=json.dumps(
-                {
-                    "status": "User added",
-                    "user_id": self.user_id,
-                    "current_users": self.user_list,
-                }
-            )
-        )
+        if self.user_id not in self.user_list:
+            self.user_list.append(self.user_id)
+        await self.broadcast_user_list("User added")
 
     async def remove_user_from_list(self):
-        # ユーザーリストから削除
         if self.user_id in self.user_list:
             self.user_list.remove(self.user_id)
+        await self.broadcast_user_list("User removed")
 
+    async def broadcast_user_list(self, status):
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type": "send_user_list",
+                "status": status,
+                "user_id": self.user_id,
+                "current_users": self.user_list,
+            },
+        )
+
+    async def send_user_list(self, event):
         await self.send(
             text_data=json.dumps(
                 {
-                    "status": "User removed",
-                    "user_id": self.user_id,
-                    "current_users": self.user_list,
+                    "status": event["status"],
+                    "user_id": event["user_id"],
+                    "current_users": event["current_users"],
                 }
             )
         )
