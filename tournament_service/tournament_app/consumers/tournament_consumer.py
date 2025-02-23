00d7496dc188a0dc10ac2e2afe89ef83aaa0ab0e
rikeda-cloud <longsichitian648@gmail.com>
2025-02-23 14:51:46 +0900

    [update] connect時、試合状況データを送信する処理の追加

--- tournament_service/tournament_app/consumers/tournament_consumer.py
+++ tournament_service/tournament_app/consumers/tournament_consumer.py
@@ -1,4 +1,8 @@
+import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from tournament_app.utils.tournament_session import TournamentSession
 
 
 class TournamentConsumer(AsyncWebsocketConsumer):
@@ -8,11 +12,19 @@ class TournamentConsumer(AsyncWebsocketConsumer):
         # tournamentIdに紐づいたルーム
         self.room_group_name = f"tournament_{self.tournament_id}"
 
+        # TournamentSessionが存在しない場合、接続を拒否する
+        tournament_session = TournamentSession.search(self.tournament_id)
+        if tournament_session is None:
+            await self.close(code=4400)
+            return
+
         # WebSocket グループに参加
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
-        # TODO Tournamentの状況をSendする処理を追加
+        # 試合状況をSend
+        matches_data = tournament_session.matches_data
+        await self.send(text_data=json.dumps(matches_data))
 
     async def disconnect(self, _):
         # WebSocket グループから退出
