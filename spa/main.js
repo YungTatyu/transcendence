c04e180e2cd0273f6dce7d687430c3abb16891de
tk1225 <takuxoka1225@gmail.com>
2025-03-02 15:40:10 +0900

    fix import

--- spa/main.js
+++ spa/main.js
@@ -1,10 +1,10 @@
 import SPA from "./spa.js";
 window.SPA = SPA;
 
-import ApiData, { setupApiData } from "./views/apiPage.js";
 import Home from "./views/Home.js";
 import NotFound from "./views/NotFound.js";
 import Store, { setupStore } from "./views/Store.js";
+import ApiData, { setupApiData } from "./views/apiPage.js";
 
 SPA.route("/", Home);
 SPA.route("/404", NotFound);
