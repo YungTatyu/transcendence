28cba29913f66ef5d6798ee0c7fc71ee830c330f
rikeda-cloud <longsichitian648@gmail.com>
2025-03-02 18:57:16 +0900

    [update] TournamentConsumerのテストの雛形を作成

--- tournament_service/tournament_app/tests/conftest.py
+++ tournament_service/tournament_app/tests/conftest.py
@@ -1,4 +1,7 @@
 import pytest
+from unittest.mock import AsyncMock, MagicMock, patch
+
+from tournament_app.utils.tournament_session import TournamentSession
 
 
 @pytest.fixture()
@@ -26,3 +29,74 @@ def update_matches_data_mocker(mocker):
         "tournament_app.utils.tournament_session.TournamentSession.update_matches_data",
         side_effect=None,
     )
+
+
+@pytest.fixture()
+def dummy_matches_data_start_mocker(mocker):
+    """
+    参加者2人の場合のmatches_dataをreturn
+    """
+    dummy_matches_data = {
+        "matches_data": [
+            {
+                "matchId": 2,
+                "winnerUserId": 40014,
+                "mode": "Tournament",
+                "tournamentId": 1,
+                "parentMatchId": None,
+                "round": 1,
+                "participants": [
+                    {"id": 40014, "score": None},
+                    {"id": 40046, "score": None},
+                ],
+            },
+        ],
+        "current_round": 1,
+        "state": "ongoing",
+    }
+    return mocker.patch(
+        "tournament_app.utils.tournament_session.TournamentSession.update_matches_data",
+        return_value=dummy_matches_data,
+    )
+
+
+@pytest.fixture()
+def dummy_matches_data_end_mocker(mocker):
+    """
+    参加者2人の場合のmatches_dataをreturn
+    """
+    dummy_matches_data = {
+        "matches_data": [
+            {
+                "matchId": 2,
+                "winnerUserId": 40014,
+                "mode": "Tournament",
+                "tournamentId": 1,
+                "parentMatchId": None,
+                "round": 1,
+                "participants": [{"id": 40014, "score": 0}, {"id": 40046, "score": -1}],
+            },
+        ],
+        "current_round": 2,
+        "state": "finished",
+    }
+    return mocker.patch(
+        "tournament_app.utils.tournament_session.TournamentSession.update_matches_data",
+        return_value=dummy_matches_data,
+    )
+
+
+@pytest.fixture()
+def mock_limit_tournament_match_sec(mocker):
+    """TournamentSession.LIMIT_TOURNAMENT_MATCH_SEC をモックして値を変更"""
+    mocker.patch.object(TournamentSession, "LIMIT_TOURNAMENT_MATCH_SEC", 1)
+
+
+@pytest.fixture
+def mock_handle_tournament_match_bye():
+    with patch.object(
+        TournamentSession,
+        "handle_tournament_match_bye",
+        new=TournamentSession.update_tournament_session_info,
+    ):
+        yield
---
+++ tournament_service/tournament_app/tests/test_tournament_consumer.py
@@ -0,0 +1,72 @@
+import asyncio
+import pytest
+from channels.testing import WebsocketCommunicator
+
+from tournament_app.consumers.tournament_matching_consumer import (
+    TournamentMatchingConsumer as Tmc,
+)
+from tournament_app.consumers.tournament_consumer import TournamentConsumer
+from tournament_app.utils.tournament_session import TournamentSession
+
+PATH_MATCHING = "/tournament/ws/enter-room"
+FORMAT_TOURNAMENT = "/tournament/ws/enter-room/{}"
+
+
+class CustomWebsocketCommunicator(WebsocketCommunicator):
+    def __init__(self, application, path, scope_override=None):
+        super().__init__(application, path)
+        if scope_override:
+            self.scope.update(scope_override)
+
+
+async def create_communicator(port: int):
+    communicator = CustomWebsocketCommunicator(
+        Tmc.as_asgi(),
+        PATH_MATCHING,
+        scope_override={"client": ("127.0.0.1", port)},
+    )
+    connected, _ = await communicator.connect()
+    assert connected
+    return communicator
+
+
+async def create_tournament_communicator(tournament_id: int):
+    path = FORMAT_TOURNAMENT.format(tournament_id)
+    communicator = WebsocketCommunicator(TournamentConsumer.as_asgi(), path)
+    communicator.scope["url_route"] = {"kwargs": {"tournamentId": tournament_id}}
+    connected, _ = await communicator.connect()
+    assert connected
+    return communicator
+
+
+@pytest.mark.asyncio(loop_scope="function")
+@pytest.mark.django_db
+async def test_create_resource(
+    create_match_records_mocker,
+    dummy_matches_data_start_mocker,
+    mock_handle_tournament_match_bye,
+):
+    # トーナメントマッチングルームに入り、tournament_idを取得
+    comms = []
+    for i in range(Tmc.ROOM_CAPACITY):
+        comms.append(await create_communicator(10000 + i))
+        [await communicator.receive_json_from() for communicator in comms]
+
+    tournament_id = await comms[0].receive_json_from()
+    tournament_id = int(tournament_id["tournament_id"])
+
+    for communicator in comms:
+        await communicator.disconnect()
+
+    # トーナメントルームに入り、試合が展開される
+    tournament_comms = []
+    for _ in range(Tmc.ROOM_CAPACITY):
+        communicator = await create_tournament_communicator(tournament_id)
+        res = await communicator.receive_json_from()
+        tournament_comms.append(communicator)
+        print("\n\n[1]", res)
+
+    await asyncio.sleep(TournamentSession.LIMIT_TOURNAMENT_MATCH_SEC + 1)
+    for communicator in tournament_comms:
+        res = await communicator.receive_json_from()
+        print("\n\n[2]", res)
