a6efe2288111545e2e6f01c26990e0bbdf7b35ec
rikeda-cloud <longsichitian648@gmail.com>
2025-03-03 15:35:18 +0900

    [add] TestCase TaskTimerによるトーナメント自動実行、tournament/finish-matchエンドポイントを叩くことによる手動実行、両方の実行方式を組み合わせたケース

--- tournament_service/tournament_app/tests/test_tournament_consumer.py
+++ tournament_service/tournament_app/tests/test_tournament_consumer.py
@@ -42,14 +42,9 @@ async def create_tournament_communicator(tournament_id: int):
     return communicator
 
 
-@pytest.mark.asyncio(loop_scope="function")
-@pytest.mark.django_db
-async def test_tournament_4_participants(
-    create_match_records_mocker,
-    dummy_matches_data_mocker,
-    mock_handle_tournament_match_bye,
-):
-    # トーナメントマッチングルームに入り、tournament_idを取得
+@pytest.fixture()
+async def setup_finished_matching(mock_limit_tournament_match_sec) -> int:
+    """トーナメントマッチングルームに入り、マッチング終了後tournament_idを取得"""
     comms = []
     for i in range(Tmc.ROOM_CAPACITY):
         comms.append(await create_communicator(10000 + i))
@@ -61,18 +56,148 @@ async def test_tournament_4_participants(
     for communicator in comms:
         await communicator.disconnect()
 
-    # トーナメントルームに入り、試合が展開される
+    return tournament_id
+
+
+@pytest.mark.django_db
+async def request_tournament_match_finish_imitate(tournament_id, round):
+    """トーナメント試合終了APIを叩く処理を模倣する"""
+    tournament_session = TournamentSession.search(tournament_id)
+    if tournament_session is None:
+        raise Exception
+    if tournament_session.current_round != round:
+        raise Exception
+    await tournament_session.update_tournament_session_info()
+
+
+@pytest.mark.asyncio(loop_scope="function")
+@pytest.mark.django_db
+async def test_tournament_auto_execution(
+    create_match_records_mocker,
+    dummy_matches_data_mocker,
+    mock_handle_tournament_match_bye,
+    mock_limit_tournament_match_sec,
+    setup_finished_matching,
+):
+    """強制勝ち上がり処理によって自動的にトーナメントが進行"""
+    tournament_id = setup_finished_matching
     tournament_comms = []
     for _ in range(Tmc.ROOM_CAPACITY):
         communicator = await create_tournament_communicator(tournament_id)
-        res = await communicator.receive_json_from()
         tournament_comms.append(communicator)
-        print("\n\n[1]", res)
+        res = await communicator.receive_json_from()
+        assert res["current_round"] == 1
+        assert res["state"] == "ongoing"
 
-    await asyncio.sleep(TournamentSession.LIMIT_TOURNAMENT_MATCH_SEC + 1)
+    # round1の強制勝ち上がり処理が自動実行される
+    await asyncio.sleep(TournamentSession.LIMIT_TOURNAMENT_MATCH_SEC + 0.1)
     for communicator in tournament_comms:
         res = await communicator.receive_json_from()
-        print("\n\n[2]", res)
+        assert res["current_round"] == 2
+        assert res["state"] == "ongoing"
+
+    # round2の強制勝ち上がり処理が自動実行される
+    await asyncio.sleep(TournamentSession.LIMIT_TOURNAMENT_MATCH_SEC + 0.1)
+    for communicator in tournament_comms:
+        res = await communicator.receive_json_from()
+        assert res["current_round"] == 3
+        assert res["state"] == "ongoing"
+
+    # round3(決勝戦)の強制勝ち上がり処理が自動実行される
+    await asyncio.sleep(TournamentSession.LIMIT_TOURNAMENT_MATCH_SEC + 0.1)
+    for communicator in tournament_comms:
+        res = await communicator.receive_json_from()
+        assert res["state"] == "finished"
+
+    for communicator in tournament_comms:
+        await communicator.disconnect()
+    TournamentSession.clear()
+
+
+@pytest.mark.asyncio(loop_scope="function")
+@pytest.mark.django_db
+async def test_tournament_manual_execution(
+    create_match_records_mocker,
+    dummy_matches_data_mocker,
+    mock_handle_tournament_match_bye,
+    setup_finished_matching,
+):
+    """tournaments/finish-matchエンドポイントを叩くことよってトーナメントが進行"""
+    tournament_id = setup_finished_matching
+    tournament_comms = []
+    for _ in range(Tmc.ROOM_CAPACITY):
+        communicator = await create_tournament_communicator(tournament_id)
+        tournament_comms.append(communicator)
+        res = await communicator.receive_json_from()
+        assert res["current_round"] == 1
+        assert res["state"] == "ongoing"
+
+    # round1の手動トーナメント試合終了処理で次の試合に進む
+    await request_tournament_match_finish_imitate(tournament_id, 1)
+    for communicator in tournament_comms:
+        res = await communicator.receive_json_from()
+        assert res["current_round"] == 2
+        assert res["state"] == "ongoing"
+
+    # round2の手動トーナメント試合終了処理で次の試合に進む
+    await request_tournament_match_finish_imitate(tournament_id, 2)
+    for communicator in tournament_comms:
+        res = await communicator.receive_json_from()
+        assert res["current_round"] == 3
+        assert res["state"] == "ongoing"
+
+    # round3(決勝戦)の手動トーナメント試合終了処理で次の試合に進む
+    await request_tournament_match_finish_imitate(tournament_id, 3)
+    for communicator in tournament_comms:
+        res = await communicator.receive_json_from()
+        assert res["state"] == "finished"
+
+    for communicator in tournament_comms:
+        await communicator.disconnect()
+    TournamentSession.clear()
+
+
+@pytest.mark.asyncio(loop_scope="function")
+@pytest.mark.django_db
+async def test_tournament_manual_and_auto_execution(
+    create_match_records_mocker,
+    dummy_matches_data_mocker,
+    mock_handle_tournament_match_bye,
+    mock_limit_tournament_match_sec,
+    setup_finished_matching,
+):
+    """
+    round1はtournaments/finish-matchが叩かれることによる手動処理
+    round2,round3はTaskTimerによる自動勝ち上がり処理
+    """
+    tournament_id = setup_finished_matching
+    tournament_comms = []
+    for _ in range(Tmc.ROOM_CAPACITY):
+        communicator = await create_tournament_communicator(tournament_id)
+        tournament_comms.append(communicator)
+        res = await communicator.receive_json_from()
+        assert res["current_round"] == 1
+        assert res["state"] == "ongoing"
+
+    # round1の手動トーナメント試合終了処理で次の試合に進む
+    await request_tournament_match_finish_imitate(tournament_id, 1)
+    for communicator in tournament_comms:
+        res = await communicator.receive_json_from()
+        assert res["current_round"] == 2
+        assert res["state"] == "ongoing"
+
+    # round2の強制勝ち上がり処理が自動実行される
+    await asyncio.sleep(TournamentSession.LIMIT_TOURNAMENT_MATCH_SEC + 0.1)
+    for communicator in tournament_comms:
+        res = await communicator.receive_json_from()
+        assert res["current_round"] == 3
+        assert res["state"] == "ongoing"
+
+    # round3(決勝戦)の強制勝ち上がり処理が自動実行される
+    await asyncio.sleep(TournamentSession.LIMIT_TOURNAMENT_MATCH_SEC + 0.1)
+    for communicator in tournament_comms:
+        res = await communicator.receive_json_from()
+        assert res["state"] == "finished"
 
     for communicator in tournament_comms:
         await communicator.disconnect()
