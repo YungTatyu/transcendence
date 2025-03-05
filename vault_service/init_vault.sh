862ed3c6640ba1e80b6328e474fc752a9041a83a
rikeda-cloud <longsichitian648@gmail.com>
2025-03-05 18:54:56 +0900

    [add] APIキー配信用エンドポイント設定を追加

--- vault_service/init_vault.sh
+++ vault_service/init_vault.sh
@@ -2,8 +2,8 @@
 
 # Vaultサーバの起動待機
 until vault status 2>&1 | grep -q "Initialized"; do
-  echo "Vault起動待機中..."
-  sleep 2
+	echo "Vault起動待機中..."
+	sleep 2
 done
 
 # Vaultの初期化
@@ -19,14 +19,20 @@ vault secrets enable transit
 vault write -f transit/keys/jwt-key type=rsa-2048
 vault read transit/keys/jwt-key
 
+# APIキー配信用の設定
+AUTO_ROTATE_PERIOD="auto_rotate_period=24h"
+vault write transit/keys/api-key-users ${AUTO_ROTATE_PERIOD}
+vault write transit/keys/api-key-matches ${AUTO_ROTATE_PERIOD}
+vault write transit/keys/api-key-tournaments ${AUTO_ROTATE_PERIOD}
+
 # ポリシーを作成
 vault policy write transit-policy /vault/config/transit-policy.hcl
 
 # TLS認証の設定
 vault auth enable cert
 vault write auth/cert/certs/client \
-    display_name="client" \
-    policies="default,transit-policy" \
-    certificate="$(cat /vault/certs/ca.crt)"
+	display_name="client" \
+	policies="default,transit-policy" \
+	certificate="$(cat /vault/certs/ca.crt)"
 
 echo "Vault ルートトークン: $root_token"
