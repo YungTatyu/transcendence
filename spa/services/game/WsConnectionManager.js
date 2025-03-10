f642e6778c99b0acf9b07b8dff995d622914f094
tachu <roatyungtatyu@gmail.com>
2025-03-10 20:14:47 +0900

    add: WsConnectionManager

--- spa/services/game/WsConnectionManager.js
+++ spa/services/game/WsConnectionManager.js
@@ -62,13 +62,36 @@ const WsConnectionManager = {
   socket: null,
   eventHandler: wsEventHandler,
 
-  connect(matchId) { },
+  connect(matchId) {
+    this.socket = new WebSocket(`ws://127.0.0.1:8002/games/ws/enter-room/${matchId}`)
+  },
 
-  disconnect() { },
+  disconnect() {
+    if (this.socket === null) {
+      return;
+    }
+    this.socket.close();
+    this.socket = null;
+  },
 
-  sendMessage(message) { },
+  sendMessage(message) {
+    if (!this.socket || this.socket.readyState !== WebSocket.OPEN) {
+      console.error("Socket is not open. Unable to send message.");
+      return;
+    }
+    this.socket.send(message);
+  },
 
-  registerEventHandler() { },
+  registerEventHandler() {
+    if (!this.socket || this.socket.readyState !== WebSocket.OPEN) {
+      console.error("Socket is not open. Unable to send message.");
+      return;
+    }
+    this.socket.onopen = this.eventHandler.handleOpen.bind(this.eventHandler);
+    this.socket.onmessage = this.eventHandler.handleMessage.bind(this.eventHandler);
+    this.socket.onclose = this.eventHandler.handleClose.bind(this.eventHandler);
+    this.socket.onerror = this.eventHandler.handleError.bind(this.eventHandler);
+  },
 };
 
 export default WsConnectionManager;
