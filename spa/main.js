6016bab4cb06a770b674b80722f358a359a1198a
tachu <roatyungtatyu@gmail.com>
2025-03-11 13:02:50 +0900

    fix: fmt

--- spa/main.js
+++ spa/main.js
@@ -1,11 +1,11 @@
 import SPA from "./spa.js";
 window.SPA = SPA;
 
+import Game, { setupGame } from "./views/Game.js";
 import Home from "./views/Home.js";
 import NotFound from "./views/NotFound.js";
 import Store, { setupStore } from "./views/Store.js";
 import ApiData, { setupApiData } from "./views/apiPage.js";
-import Game, { setupGame } from "./views/Game.js";
 
 SPA.route("/", Home);
 SPA.route("/404", NotFound);
