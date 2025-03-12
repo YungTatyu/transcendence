9793d446b3b89d05c8aec640f911b7fe44064b6c
rikeda-cloud <longsichitian648@gmail.com>
2025-03-12 18:52:52 +0900

    [refactor] print_log_and_exit関数にデフォルト値を設定

--- vault_service/init_vault_and_rotate_api_key.sh
+++ vault_service/init_vault_and_rotate_api_key.sh
@@ -2,7 +2,7 @@
 
 print_log_and_exit() {
 	local message=$1
-	local exit_code=$2
+	local exit_code=${2:-1} # デフォルト値を1に設定
 	echo "[$(date '+%Y-%m-%dT%H:%M:%S%z')]: $message" >&2
 	exit $exit_code
 }
@@ -18,7 +18,7 @@ wait_for_vault() {
 		((elapsed += 2))
 
 		if [ "$elapsed" -ge "$timeout" ]; then
-			print_log_and_exit "Vaultの起動を60秒待ちましたが、応答がありません。" 1
+			print_log_and_exit "Vaultの起動を60秒待ちましたが、応答がありません。"
 		fi
 	done
 }
@@ -33,27 +33,27 @@ initialize_vault() {
 
 	# Vaultアンシール
 	if ! vault operator unseal "$unseal_key"; then
-		print_log_and_exit "Vaultアンシールに失敗しました" 1
+		print_log_and_exit "Vaultアンシールに失敗しました"
 	fi
 }
 
 # Vault設定: transitシークレットエンジンの有効化と署名キー作成
 enable_transit() {
 	if ! vault secrets enable transit; then
-		print_log_and_exit "transitシークレットエンジンの有効化に失敗しました" 1
+		print_log_and_exit "transitシークレットエンジンの有効化に失敗しました"
 	fi
 	if ! vault write -f transit/keys/jwt-key type=rsa-2048; then
-		print_log_and_exit "署名キーの作成に失敗しました" 1
+		print_log_and_exit "署名キーの作成に失敗しました"
 	fi
 	if ! vault read transit/keys/jwt-key; then
-		print_log_and_exit "JWTキーの読み取りに失敗しました" 1
+		print_log_and_exit "JWTキーの読み取りに失敗しました"
 	fi
 }
 
 # APIキー配信用の設定
 enable_api_keys() {
 	if ! vault secrets enable -path=kv/apikeys -description="kv for APIKey" kv; then
-		print_log_and_exit "APIキーのkv設定に失敗しました" 1
+		print_log_and_exit "APIキーのkv設定に失敗しました"
 	fi
 }
 
@@ -61,22 +61,22 @@ enable_api_keys() {
 enable_tls_auth() {
 	# ポリシーを作成
 	if ! vault policy write transit-policy /vault/config/transit-policy.hcl; then
-		print_log_and_exit "transit-policy の適用に失敗しました" 1
+		print_log_and_exit "transit-policy の適用に失敗しました"
 	fi
 
 	if ! vault policy write kv-policy /vault/config/kv-policy.hcl; then
-		print_log_and_exit "kv-policy の適用に失敗しました" 1
+		print_log_and_exit "kv-policy の適用に失敗しました"
 	fi
 
 	if ! vault auth enable cert; then
-		print_log_and_exit "TLS認証の設定に失敗しました" 1
+		print_log_and_exit "TLS認証の設定に失敗しました"
 	fi
 
 	if ! vault write auth/cert/certs/client \
 		display_name="client" \
 		policies="default,transit-policy,kv-policy" \
 		certificate="$(cat /vault/certs/ca.crt)"; then
-		print_log_and_exit "TLS証明書の設定に失敗しました" 1
+		print_log_and_exit "TLS証明書の設定に失敗しました"
 	fi
 }
 
@@ -92,12 +92,12 @@ update_api_key() {
 
 	# APIキー更新の前に引数チェック
 	if [ -z "$key_name" ] || [ -z "$old_key" ] || [ -z "$new_key" ]; then
-		print_log_and_exit "無効な引数が渡されました" 1
+		print_log_and_exit "無効な引数が渡されました"
 	fi
 
 	# 新しいAPIキーを保存し、前のAPIキーを previous_value に保存
 	if ! vault kv put kv/apikeys/$key_name value="$new_key" previous_value="$old_key"; then
-		print_log_and_exit "APIキー '$key_name' の更新に失敗しました。" 1
+		print_log_and_exit "APIキー '$key_name' の更新に失敗しました。"
 	fi
 }
 
