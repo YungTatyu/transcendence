48dc949c4811b64c9719382d03ec6470b53d8708
kealoke <yuna.kealoke@gmail.com>
2025-03-13 21:43:48 +0900

    [fix] import api data last

--- spa/main.js
+++ spa/main.js
@@ -1,7 +1,6 @@
 import SPA from "./spa.js";
 window.SPA = SPA;
 
-import ApiData, { setupApiData } from "./views/apiPage.js";
 import Game, { setupGame } from "./views/Game.js";
 import Home from "./views/Home.js";
 import Login, { setupLogin } from "./views/Login.js";
@@ -12,6 +11,7 @@ import Profile from "./views/Profile.js";
 import SignUp, { setupSignUp } from "./views/SignUp.js";
 import SignUpVerify, { setupSignUpVerify } from "./views/SignUpVerify.js";
 import Store, { setupStore } from "./views/Store.js";
+import ApiData, { setupApiData } from "./views/apiPage.js";
 
 SPA.route("/", Home);
 SPA.route("/404", NotFound);
