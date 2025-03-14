738e59f54de0e566cd64ef1a7ad40066b04bf0c9
rikeda-cloud <longsichitian648@gmail.com>
2025-03-14 19:05:11 +0900

    [update] クラス型テストケース -> 関数型テストケース

--- match_service/match_app/tests/test_quick_play_consumer.py
+++ match_service/match_app/tests/test_quick_play_consumer.py
@@ -3,56 +3,67 @@ from datetime import timedelta
 import jwt
 import pytest
 from channels.testing import WebsocketCommunicator
-from django.test import TestCase
 from config.asgi import application
 from match_app.utils.quick_play_matching_manager import QuickPlayMatchingManager
+from match_app.consumers.quick_play_consumer import QuickPlayConsumer
 
 PATH_MATCHING = "/matches/ws/enter-room"
 
 
+def create_jwt_for_user(user_id):
+    # JWTを生成するロジック
+    payload = {
+        "user_id": user_id,
+        "exp": timedelta(days=1).total_seconds(),
+        "iat": timedelta(days=0).total_seconds(),
+    }
+    secret_key = "your_secret_key"
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
+    return token
+
+
+async def create_communicator(user_id: int, expect_connected=True):
+    """JWTをCookieに含んでWebSocketコネクションを作成"""
+    access_token = create_jwt_for_user(user_id)
+    communicator = WebsocketCommunicator(application, PATH_MATCHING)
+    communicator.scope["cookies"] = {"access_token": access_token}
+    connected, _ = await communicator.connect()
+    assert connected == expect_connected
+    return communicator
+
+
 @pytest.mark.asyncio(loop_scope="function")
-class TestQuickPlayConsumer(TestCase):
-    def setUp(self):
-        QuickPlayMatchingManager.clear_waiting_users()
+@pytest.mark.django_db
+async def test_websocket_connect():
+    comms = []
+    for i in range(QuickPlayConsumer.ROOM_CAPACITY):
+        user_id = i + 1
+        communicator = await create_communicator(user_id)
+        comms.append(communicator)
 
-    def create_jwt_for_user(self, user_id):
-        # JWTを生成するロジック
-        payload = {
-            "user_id": user_id,
-            "exp": timedelta(days=1).total_seconds(),
-            "iat": timedelta(days=0).total_seconds(),
-        }
-        secret_key = "your_secret_key"
-        token = jwt.encode(payload, secret_key, algorithm="HS256")
-        return token
+    for communicator in comms:
+        res = await communicator.receive_json_from()
+        assert res.get("match_id", None) is not None
 
-    async def create_communicator(self, user_id: int, expect_connected=True):
-        """JWTをCookieに含んでWebSocketコネクションを作成"""
-        access_token = self.create_jwt_for_user(user_id)
-        communicator = WebsocketCommunicator(application, PATH_MATCHING)
-        communicator.scope["cookies"] = {"access_token": access_token}
-        connected, _ = await communicator.connect()
-        assert connected == expect_connected
-        return communicator
+    [await communicator.disconnect() for communicator in comms]
+    QuickPlayMatchingManager.clear_waiting_users()
 
-    async def test_websocket_connect(self):
-        user_id = 1
 
-        communicator = await self.create_communicator(user_id)
+@pytest.mark.asyncio(loop_scope="function")
+async def test_has_not_jwt():
+    """コネクション確立時にJWTを含まないケースはコネクションが確立できない"""
+    communicator = WebsocketCommunicator(application, PATH_MATCHING)
+    connected, _ = await communicator.connect()
+    assert not connected
+    QuickPlayMatchingManager.clear_waiting_users()
 
-        # 接続解除
-        await communicator.disconnect()
 
-    async def test_has_not_jwt(self):
-        """コネクション確立時にJWTを含まないケースはコネクションが確立できない"""
-        communicator = WebsocketCommunicator(application, PATH_MATCHING)
-        connected, _ = await communicator.connect()
-        assert not connected
+@pytest.mark.asyncio(loop_scope="function")
+async def test_enter_room_same_user():
+    """同一ユーザーがマッチングルームに入った場合、コネクションが確立できない"""
+    user_id = 1
 
-    async def test_enter_room_same_user(self):
-        """同一ユーザーがマッチングルームに入った場合、コネクションが確立できない"""
-        user_id = 1
-
-        communicator = await self.create_communicator(user_id)
-        await self.create_communicator(user_id, expect_connected=False)
-        await communicator.disconnect()
+    communicator = await create_communicator(user_id)
+    await create_communicator(user_id, expect_connected=False)
+    await communicator.disconnect()
+    QuickPlayMatchingManager.clear_waiting_users()
