0d5fb196fa7480b15dab6232feb9d894863a027b
rikeda-cloud <longsichitian648@gmail.com>
2025-03-15 16:18:06 +0900

    [add] 試合参加資格のないユーザーを弾く処理を追加

--- match_service/match_app/consumers/tournament_match_consumer.py
+++ match_service/match_app/consumers/tournament_match_consumer.py
@@ -1,4 +1,6 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+from match_app.models import Match, MatchParticipant
+from channels.db import database_sync_to_async
 
 
 class TournamentMatchConsumer(AsyncWebsocketConsumer):
@@ -10,13 +12,18 @@ class TournamentMatchConsumer(AsyncWebsocketConsumer):
         return cls.GROUP_NAME_FORMAT.format(match_id)
 
     async def connect(self):
+        self.user_id = self.scope.get("user_id")
+        if not self.user_id:
+            await self.close()
+            return
+
         self.match_id = int(self.scope["url_route"]["kwargs"]["matchId"])
         self.room_group_name = TournamentMatchConsumer.get_group_name(self.match_id)
 
-        # TODO participants = get_match_data()
-        # if not self.user_id in participants:
-        #     await self.close(code=4400)
-        #     return
+        participant_ids = self.__select_match_participant_ids(self.match_id)
+        if self.user_id not in participant_ids:
+            await self.close(code=4400)
+            return
 
         # WebSocket グループに参加
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
@@ -25,3 +32,19 @@ class TournamentMatchConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, _):
         pass
+
+    @database_sync_to_async
+    def __select_match_participant_ids(self, match_id: int) -> list[int]:
+        match = Match.objects.filter(match_id=match_id)
+
+        if (
+            not match  # 試合が存在しない
+            or match.finish_date is not None  # 試合が既に終了している
+            or match.mode != "Tournament"  # 試合がTournamentの試合ではない
+        ):
+            return []
+
+        participant_ids = []
+        for participant in MatchParticipant.objects.filter(match_id=match):
+            participant_ids.append(participant.user_id)
+        return participant_ids
