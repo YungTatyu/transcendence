f43815d4139874d954a3f68641906e85367f5c6d
rikeda-cloud <longsichitian648@gmail.com>
2025-03-15 14:11:14 +0900

    [del] tournament matching関連の処理を削除

--- match_service/match_app/routing.py
+++ match_service/match_app/routing.py
@@ -1,12 +1,7 @@
 from django.urls import re_path
 
 from match_app.consumers.quick_play_consumer import QuickPlayConsumer
-from match_app.consumers.tournament_match_consumer import TournamentMatchConsumer
 
 websocket_urlpatterns = [
     re_path(r"^matches/ws/enter-room$", QuickPlayConsumer.as_asgi()),
-    re_path(
-        r"^matches/ws/enter-room/(?P<matchId>\d+)$",
-        TournamentMatchConsumer.as_asgi(),
-    ),
 ]
