255b7d69d68288e3b1352a77f89088238894a1b0
tachu <roatyungtatyu@gmail.com>
2025-03-16 15:09:32 +0900

    add: navigate

--- spa/views/Home.js
+++ spa/views/Home.js
@@ -12,6 +12,8 @@ export default function Home() {
         <button onclick="SPA.navigate('/404')">404へ</button>
         <button onclick="SPA.navigate('/signup')">signupへ</button>
         <button onclick="SPA.navigate('/login')">loginへ</button>
+        <button onclick="SPA.navigate('/game')">gameへ</button>
+        <button onclick="SPA.navigate('/game/setup')">setupgameへ</button>
         ${Footer({ text: "© 2025 My Company" })}
         <div> store sample</div>
         ${stateManager.state.count}
