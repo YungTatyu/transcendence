b40932dbc3384db3721bcd9f133054c9651ff3cd
rikeda-cloud <longsichitian648@gmail.com>
2025-03-16 16:25:32 +0900

    [update] TournamentMatchWaiter

--- match_service/match_app/utils/tournament_match_waiter.py
+++ match_service/match_app/utils/tournament_match_waiter.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from match_app.utils.task_timer import TaskTimer
 from match_app.models import MatchParticipant
+from asgiref.sync import async_to_sync
 
 
 class TournamentMatchWaiter:
@@ -13,12 +14,10 @@ class TournamentMatchWaiter:
         self.__task_timer = None
         self.__user_ids: set[int] = self.__fetch_user_ids(match_id)
         self.__connected_user_ids: set[int] = set()
-        # TODO 強制処理タイマーセット
+        async_to_sync(self.set_wait_timer, force_new_loop=False)()
 
     @classmethod
-    def register(cls, match_id: int) -> Optional["TournamentMatchWaiter"]:
-        if match_id in cls.__tournament_match_waiter_dict:
-            return None
+    def register(cls, match_id: int) -> "TournamentMatchWaiter":
         tournament_match = TournamentMatchWaiter(match_id)
         cls.__tournament_match_waiter_dict[match_id] = tournament_match
         return tournament_match
@@ -42,6 +41,25 @@ class TournamentMatchWaiter:
     def match_id(self) -> int:
         return self.__match_id
 
+    @property
+    def connected_user_ids(self) -> list[int]:
+        return list(self.__connected_user_ids)
+
+    @property
+    def is_ready(self) -> bool:
+        return len(self.__user_ids) == len(self.__connected_user_ids)
+
+    def add_user(self, user_id):
+        self.__connected_user_ids.add(user_id)
+
+    def del_user(self, user_id):
+        self.cancel_timer()
+        self.__connected_user_ids.remove(user_id)
+
+    def cancel_timer(self):
+        if self.__task_timer is not None:
+            self.__task_timer.cancel()
+
     async def set_wait_timer(self):
         """
         トーナメント試合の参加者が永遠に揃わない状況を防ぐ
@@ -51,7 +69,15 @@ class TournamentMatchWaiter:
         )
 
     async def handle_fallback_tournament_match(self):
-        pass
+        """
+        トーナメント試合に参加者全員が揃わない場合に強制的行われる処理
+        """
+        if len(self.__connected_user_ids) == 1:
+            # TODO DBにレコードを挿入し、/tournaments/finish-matchエンドポイントを叩く
+            return
+
+        # 現在いるユーザーのみにSendする
+        self.cancel_timer()
 
     def __fetch_user_ids(self, match_id: int) -> set[int]:
         participants = MatchParticipant.objects.filter(match_id=match_id)
