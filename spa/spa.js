0e5520c472c541cadd6991b4c58904e9fdcef84c
tachu <roatyungtatyu@gmail.com>
2025-03-17 15:38:42 +0900

    modify: async functions

--- spa/spa.js
+++ spa/spa.js
@@ -13,22 +13,22 @@ const SPA = (() => {
     routes[path] = { view, setup };
   };
 
-  const navigate = (path, params = null, replace = false) => {
+  const navigate = async (path, params = null, replace = false) => {
     if (replace) {
       history.replaceState({}, "", path);
     } else {
       history.pushState({}, "", path);
     }
-    renderRoute(params);
+    await renderRoute(params);
   };
 
-  const renderRoute = (params) => {
+  const renderRoute = async (params) => {
     const path = window.location.pathname;
     const route = routes[path] || routes["/404"];
     if (route && container) {
       container.innerHTML = route.view(params);
       if (route.setup) {
-        route.setup();
+        await route.setup();
       }
     }
   };
