7b3a134053d41f53db97ee818a19c95811279313
kealoke <yuna.kealoke@gmail.com>
2025-03-19 00:43:43 +0900

    [fix] スペルミス& changeavatarのsetup削除

--- spa/views/ChangeMail.js
+++ spa/views/ChangeMail.js
@@ -1,7 +1,7 @@
 import Form from "../components/Form.js";
 
 export default function ChangeMail() {
-  const ChangeMialFormField = [
+  const ChangeMailFormField = [
     { label: "Mail", type: "email", placeholder: "sample@example.com" },
   ];
   return Form(ChangeMailFormField, "changeMail", "Submit", "Set Your Mail");
