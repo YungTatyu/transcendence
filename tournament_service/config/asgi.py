b999b464bf010c8d87d4390a6cb22dfafe9d7fc6
rikeda-cloud <longsichitian648@gmail.com>
2025-03-19 17:04:03 +0900

    [add] middleware.py to tournament service

--- tournament_service/config/asgi.py
+++ tournament_service/config/asgi.py
@@ -11,8 +11,8 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from tournament_app.middleware import JWTAuthMiddleware
 from tournament_app.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -20,7 +20,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AllowedHostsOriginValidator(
+        "websocket": JWTAuthMiddleware(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
     }
