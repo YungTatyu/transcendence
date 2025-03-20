fca5c1c4803088738df92b0632d0b114c61ef1b9
rikeda-cloud <longsichitian648@gmail.com>
2025-03-20 16:08:11 +0900

    [fix] home-icon pos

--- spa/components/TitleMatchingRoom.js
+++ spa/components/TitleMatchingRoom.js
@@ -7,11 +7,9 @@ export default function TitileAndHomeButton(titleText) {
           <h1 class="title-matching-room mr-second">${titleText}</h1>
           <h1 class="title-matching-room mr-third">${titleText}</h1>
         </div>
-        <div class="col align-self-end">
           <div class="position-relative">
-            <img src="/assets/home.png" class="home-icon position-absolute top-0 end-0">
+            <img src="/assets/home.png" class="home-icon position-absolute top-0 end-0" style="margin-top: 5%;">
           </div>
-        </div>
       </div>
     </div>
   `;
