857fa34624d1c3ec5a63822f092eb04c86e13ee7
rikeda-cloud <longsichitian648@gmail.com>
2025-03-20 16:48:00 +0900

    [update] マッチング情報表示要素の追加

--- spa/views/QuickPlayMatching.js
+++ spa/views/QuickPlayMatching.js
@@ -4,6 +4,9 @@ import TitleMatchingRoom from "../components/TitleMatchingRoom.js";
 export default function QuickPlayMatching() {
 	return `
       ${TitleMatchingRoom("Quick Play")}
+      <p id="matching-info" class="d-flex justify-content-center align-items-center">
+        LOOKING FOR AN OPPONENT.
+      </p>
       ${MatchingRoom()}
 	`;
 }
