98a32cafd0008b0b4fc5c0c53115822e2f3aebea
Kazuki Asaka <akazuki@Kazukinonotobukkukonpyuta.local>
2025-03-24 18:08:39 +0900

    [fix] friend -> friends

--- spa/views/FriendList.js
+++ spa/views/FriendList.js
@@ -4,7 +4,7 @@ import stateManager from "../stateManager.js";
 export default function FriendList() {
   return `
 	<div class="container">
-		${TitileAndHomeButton("FRIEND LIST")}
+		${TitileAndHomeButton("FRIENDS LIST")}
 		<div class="button-position d-flex flex-column justify-content-center align-items-end gap-3">
 			<button type="button" class="find-button btn btn-primary">+ Find Friend</button>
 			<button type="button" class="request-button btn btn-primary">+ Request Friend</button>
