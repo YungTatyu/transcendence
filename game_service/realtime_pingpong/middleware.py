ff246e4b767a1c6d0564818b3c6b3e39dbd47fbf
tachu <roatyungtatyu@gmail.com>
2025-03-24 17:35:02 +0900

    add: log

--- game_service/realtime_pingpong/middleware.py
+++ game_service/realtime_pingpong/middleware.py
@@ -1,4 +1,7 @@
+import logging
 import jwt
+
+logger = logging.getLogger(__name__)
 
 
 class JWTAuthMiddleware:
@@ -8,7 +11,9 @@ class JWTAuthMiddleware:
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
+        logger.debug("starting to build ws connection")
         token = scope.get("cookies", {}).get("access_token")
+        logger.debug(f"access token: {token}")
 
         if token is None:
             await send({"type": "websocket.close", "code": 1008})
@@ -21,6 +26,8 @@ class JWTAuthMiddleware:
             scope["user_id"] = str(decoded_token.get("user_id"))
         except (jwt.ExpiredSignatureError, jwt.InvalidTokenError, jwt.DecodeError):
             await send({"type": "websocket.close", "code": 1008})
+            logger.warn("jwt auth failed")
             return
 
+        logger.debug("jwt auth success")
         return await self.inner(scope, receive, send)
