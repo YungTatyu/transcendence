92bfef915be6efb21d059ac8dc9e37797ffb04af
tachu <roatyungtatyu@gmail.com>
2025-03-25 15:56:44 +0900

    add: extension

--- spa/config.js
+++ spa/config.js
@@ -2,7 +2,7 @@ const config = {
   development: {
     userService: "http://localhost:9000",
     matchService: "http://localhost:8003",
-    gameService: "http://localhost:8001",
+    gameService: "https://game:8001",
     realtimeGameService: "wss://game:8001",
     authService: "https://auth:8000",
   },
