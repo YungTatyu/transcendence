d59e321ff37f7246e95d0e8c8ceff9532b5791f2
tachu <roatyungtatyu@gmail.com>
2025-03-26 14:44:51 +0900

    add: create_all_certs.sh

---
+++ certs/create_all_certs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+readonly SCRIPT_DIR=$(dirname $0)
+readonly CREATE_CERT=${SCRIPT_DIR}/create_cert.sh
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+main() {
+  services=(
+    "auth_app"
+    "game"
+    "tournament"
+    "user_app"
+    "friends_activity_app"
+    "friends"
+    "match"
+    "vault"
+    "auth_proxy_server"
+    "game_proxy_server"
+    "tournament_proxy_server"
+    "user_proxy_server"
+    "friends_activity_proxy_server"
+    "friends_proxy_server"
+    "match_proxy_server"
+    "spa_nginx"
+  )
+  for service in "${services[@]}"; do
+    ${CREATE_CERT} $service || err "$service cert creation failed"
+  done
+  return 0
+}
+
+main "$@"
