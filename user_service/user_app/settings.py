e460757c70176d5e63a59655d449ea1b03cdb5de
rikeda-cloud <longsichitian648@gmail.com>
2025-04-03 17:12:36 +0900

    [refactor] ALLOWED_HOSTSのuserを削除

--- user_service/user_app/settings.py
+++ user_service/user_app/settings.py
@@ -27,10 +27,7 @@ SECRET_KEY = "django-insecure-(6$k27bfa80g@9sops2j0my@8l4h@i652dw65u*=5odt2w3%kf
 DEBUG = True
 
 
-ALLOWED_HOSTS = [
-    os.getenv("PROXY"),
-    "user",
-]
+ALLOWED_HOSTS = [os.getenv("PROXY")]
 
 
 # Application definition
