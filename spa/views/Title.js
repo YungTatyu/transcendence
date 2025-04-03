13ce7bf0186e2c3f27256b8a6244fdf17278a52b
kealoke <yuna.kealoke@gmail.com>
2025-04-03 14:06:45 +0900

    [fix] rename gap class

--- spa/views/Title.js
+++ spa/views/Title.js
@@ -2,7 +2,7 @@ export default function Title() {
   return `
         <div class="d-flex justify-content-center align-items-center flex-column vh-100 position-relative">
             <p class="title-text">Ping <br />Pong</p>
-            <div class="d-flex custom-gap">
+            <div class="d-flex title-custom-gap">
                 <button class="btn btn-primary btn-signup js-btn-signup" type="button">SignUp</button>
                 <button class="btn btn-primary btn-login js-btn-login" type="button">Login</button>
             </div>
