4560740b291779a8031f3e14848e73857baa0786
tk1225 <takuxoka1225@gmail.com>
2025-04-05 20:24:33 +0900

    add test_verify_signed_jwt_success

--- auth_service/auth_app/tests/test_jwt_service.py
+++ auth_service/auth_app/tests/test_jwt_service.py
@@ -2,16 +2,14 @@
 import jwt
 from django.test import TestCase
 
-from auth_app.client.vault_client import VaultClient
-from auth_app.services.jwt_service import generate_signed_jwt
+from auth_app.services.jwt_service import generate_signed_jwt, verify_signed_jwt
 from auth_app.settings import JWT_EXPIRATION
 
 
 class JwtServiceTests(TestCase):
     def test_generate_signed_jwt_success(self):
         """
-        VaultClientが正常に動作する場合、generate_signed_jwtが正しいJWTを生成することを確認。
-        実際のVaultを使ってテストする
+        generate_signed_jwtが正しいJWTを生成する。
         """
 
         signed_jwt = generate_signed_jwt("12345", expires_in=JWT_EXPIRATION)
@@ -22,3 +20,15 @@ class JwtServiceTests(TestCase):
 
         payload = jwt.decode(signed_jwt, options={"verify_signature": False})
         self.assertEqual(payload["userId"], "12345")
+
+    def test_verify_signed_jwt_success(self):
+        """
+        verify_signed_jwtが正しく署名を検証する。
+        """
+
+        # JWTを生成して検証
+        signed_jwt = generate_signed_jwt("12345", expires_in=JWT_EXPIRATION)
+        result = verify_signed_jwt(signed_jwt)
+
+        # JWTが正しく検証されることを確認
+        self.assertTrue(result)
