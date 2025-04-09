f389082dd3a34cdf941714a383818226eb6f1ba1
tachu <roatyungtatyu@gmail.com>
2025-04-10 00:40:59 +0900

    add: jwt

---
+++ friends_activity_service/friends_activity_app/client/match_api_client.py
@@ -0,0 +1,25 @@
+import logging
+
+import requests
+from game_app.settings import MATCH_SERVICE
+
+logger = logging.getLogger(__name__)
+
+
+class MatchApiClient:
+    @staticmethod
+    def send_game_result(data):
+        url = f"{MATCH_SERVICE}/matches/finish"
+        try:
+            response = requests.post(url, json=data)
+
+            if response.status_code >= 400:
+                error_detail = response.json()
+                logger.critical(
+                    f"POST {url}\n"
+                    f"status: {response.status_code}\n"
+                    f"respones: {error_detail}\n"
+                    f"Sent Data: {data}"
+                )
+        except Exception as e:
+            logger.critical(f"POST {url}\nSent Data: {data}\nerror: {e}")
--- friends_activity_service/friends_activity_app/middleware.py
+++ friends_activity_service/friends_activity_app/middleware.py
@@ -1,4 +1,9 @@
+import logging
+
 import jwt
+from utils.jwt_service import verify_signed_jwt
+
+logger = logging.getLogger(__name__)
 
 
 class JWTAuthMiddleware:
@@ -8,19 +13,31 @@ class JWTAuthMiddleware:
         self.inner = inner
 
     async def __call__(self, scope, receive, send):
-        token = scope.get("cookies", {}).get("access_token")
+        logger.debug("starting to establish ws connection")
+        subprotocols = scope.get("subprotocols", [])
+        subprotocol = subprotocols[0] if subprotocols else None
+        token = subprotocols[1] if len(subprotocols) > 1 else None
 
-        if token is None:
+        if not all([subprotocol, token]):
             await send({"type": "websocket.close", "code": 1008})
+            logger.warn("bad request: missing subprotocol or token")
             return
 
         try:
+            is_valid = verify_signed_jwt(token)
+            if not is_valid:
+                logger.error("invalid jwt")
+                await send({"type": "websocket.close", "code": 1008})
+                return
             decoded_token = jwt.decode(
                 token, options={"verify_signature": False}, algorithms=["HS256"]
             )
+            scope["subprotocol"] = subprotocol
             scope["user_id"] = str(decoded_token.get("user_id"))
-        except (jwt.ExpiredSignatureError, jwt.InvalidTokenError, jwt.DecodeError):
+        except Exception as e:
+            logger.exception(f"JWT verification failed: {e}")
             await send({"type": "websocket.close", "code": 1008})
             return
 
+        logger.debug("jwt verification success")
         return await self.inner(scope, receive, send)
