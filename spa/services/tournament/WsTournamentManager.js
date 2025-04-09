a516661225410514277cc89aa30290371ed1e611
rikeda-cloud <longsichitian648@gmail.com>
2025-04-09 17:04:18 +0900

    [update] VS情報を描画可能

--- spa/services/tournament/WsTournamentManager.js
+++ spa/services/tournament/WsTournamentManager.js
@@ -1,6 +1,32 @@
 import config from "../../config.js";
 import createTournamentData from "./createTournamentData.js";
 import { renderTournamentBracket } from "./TournamentBracket.js";
+import { renderPlayers } from "./TournamentInfo.js";
+import fetchPlayersData from "../../api/fetchPlayersData.js";
+import { renderWaitOrStart } from "../../components/WaitOrStart.js";
+import stateManager from "../../stateManager.js";
+
+async function handleTournament(matchesData, currentRound) {
+  const participantsForRound = matchesData
+    .filter((match) => match.round === currentRound)
+    .map((match) => match.participants);
+  const playersId = participantsForRound[0].map(
+    (participant) => participant.id,
+  );
+
+  const playersData = await fetchPlayersData(playersId);
+  if (!playersData) {
+    return;
+  }
+  renderPlayers(playersData[0].name, playersData[1].name);
+
+  // INFO 次の試合参加者ならSTART、そうでないならWAITを描画
+  if (playersId.includes(Number(stateManager.state.userId))) {
+    renderWaitOrStart("START", "#FFFFFF");
+  } else {
+    renderWaitOrStart("WAIT...", "#0ca5bf");
+  }
+}
 
 const wsEventHandler = {
   handleOpen(message) {
@@ -13,12 +39,22 @@ const wsEventHandler = {
       const currentRound = parsedMessage.current_round;
       const state = parsedMessage.state;
 
-      console.log("matchesData: ", matchesData);
-      console.log("currentRound: ", currentRound);
-      console.log("state: ", state);
-
       const tournamentData = createTournamentData(matchesData);
       renderTournamentBracket(tournamentData);
+
+      switch (state) {
+        case "ongoing":
+          handleTournament(matchesData, currentRound);
+          break;
+        case "error":
+          renderWaitOrStart("ERROR", "#FF0000");
+          console.error("Tournament error");
+          break;
+        case "finished":
+          renderWaitOrStart("FINISHED", "#FFFF00");
+          console.log("Tournament finished");
+          break;
+      }
     } catch (error) {
       console.error("Failed to parse WebSocket message:", error);
     }
