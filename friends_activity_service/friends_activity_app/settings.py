0c9d7474db4751558fb57f864f30a72b981eb1b8
tachu <roatyungtatyu@gmail.com>
2025-04-10 13:57:20 +0900

    fix: install order

--- friends_activity_service/friends_activity_app/settings.py
+++ friends_activity_service/friends_activity_app/settings.py
@@ -34,13 +34,13 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "daphne",
     "channels",
     "rest_framework",
     "friends_activity_app",
