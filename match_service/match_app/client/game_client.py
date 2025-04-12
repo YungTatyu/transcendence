8ba54e998d6d9de5a8a46d06e0dbce9228c2dbb0
rikeda-cloud <longsichitian648@gmail.com>
2025-04-12 14:32:54 +0900

    [update] /gamesエンドポイントを叩く処理にAPIキーを付与する処理を追加

--- match_service/match_app/client/game_client.py
+++ match_service/match_app/client/game_client.py
@@ -3,6 +3,7 @@ import ssl
 
 import aiohttp
 from config.settings import CA_CERT
+from match_app.client.vault_client import VaultClient
 
 logger = logging.getLogger(__name__)
 
@@ -27,13 +28,18 @@ class GameClient:
             "userIdList": user_ids,
         }
 
+        api_key = VaultClient.fetch_api_key_not_required_token("games")
+        if api_key is None:
+            return {"error": "Internal Server Error"}
+        headers = {"X-API-KEY": api_key}
+
         try:
             ssl_context = ssl.create_default_context()
             ssl_context.load_verify_locations(CA_CERT)
             connector = aiohttp.TCPConnector(ssl=ssl_context)
             async with (
                 aiohttp.ClientSession(connector=connector) as session,
-                session.post(url, json=body, timeout=5) as response,
+                session.post(url, json=body, headers=headers, timeout=5) as response,
             ):
                 return await response.json()
         except Exception as e:
