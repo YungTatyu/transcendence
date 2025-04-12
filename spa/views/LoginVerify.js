b3a130eefcab2ba5c52e2074c02f1361068264b8
Kazuki Asaka <akazuki@Kazukinonotobukkukonpyuta.local>
2025-04-13 01:37:58 +0900

    [add] try-catch loginVerify

--- spa/views/LoginVerify.js
+++ spa/views/LoginVerify.js
@@ -48,8 +48,12 @@ export function setupLoginVerify() {
     stateManager.setState({ userId: data.userId });
     sessionStorage.setItem("access_token", accessToken);
     scheduleRefresh(parseJwt(accessToken));
-    WsFriendActivityManager.disconnect();
-    WsFriendActivityManager.connect(accessToken);
+    try {
+      WsFriendActivityManager.disconnect();
+      WsFriendActivityManager.connect(accessToken);
+    } catch(error) {
+      console.error(error);
+    }
     SPA.navigate("/home");
   });
 }
