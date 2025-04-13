97ea6f9c38fcd2dcbe62efda38f5342fa5071578
tachu <roatyungtatyu@gmail.com>
2025-04-13 16:19:02 +0900

    modify: setting.py

--- match_service/config/settings.py
+++ match_service/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-lpa@_gys^w-zq=1b9zgh2rj=m=tu2&iu%nm-=&-$&^l_gi_bx8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     os.getenv("PROXY"),
