e62ad891d5e0eabf8dfa00c41cc39dc0627f2d9f
kealoke <yuna.kealoke@gmail.com>
2025-04-19 15:22:15 +0900

    [change] matching room style

--- spa/components/TitleMatchingRoom.js
+++ spa/components/TitleMatchingRoom.js
@@ -3,9 +3,7 @@ export default function TitleMatchingRoom(titleText) {
     <div class="container text-center">
       <div class="row">
         <div class="col align-self-start">
-          <h1 class="title-matching-room mr-first">${titleText}</h1>
-          <h1 class="title-matching-room mr-second">${titleText}</h1>
-          <h1 class="title-matching-room mr-third">${titleText}</h1>
+          <h1 class="title-matching-room">${titleText}</h1>
         </div>
           <div class="position-relative">
             <img src="/assets/home.png" class="home-icon position-absolute top-0 end-0" style="margin-top: 5%;" onclick="SPA.navigate('/home')">
