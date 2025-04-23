8507c9e01ffa11621c794a0905a000d688b4cb67
rikeda-cloud <longsichitian648@gmail.com>
2025-04-23 16:21:08 +0900

    [add] friends_activity api exporter

--- friends_activity_service/friends_activity_app/urls.py
+++ friends_activity_service/friends_activity_app/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 
 from .views import health_check
 
 urlpatterns = [
     path("health", health_check, name="health-check"),
+    path('', include('django_prometheus.urls')),
 ]
