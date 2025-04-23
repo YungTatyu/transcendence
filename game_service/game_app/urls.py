6b936888dc63719d9989a0447447e762507052fd
rikeda-cloud <longsichitian648@gmail.com>
2025-04-23 16:35:02 +0900

    [add] game api exporter

--- game_service/game_app/urls.py
+++ game_service/game_app/urls.py
@@ -1,7 +1,8 @@
 from core import views
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("games", views.GameView.as_view(), name="games"),
     path("health", views.health_check, name="health"),
+    path('', include('django_prometheus.urls')),
 ]
